4bc89f32ce70fce223475d88b5da35a85fa4fe0c
Maho Morichika <nat@magicnat.com>
2020-09-18 15:48:14 -0400

    make attributes private

--- InternetSimulator/src/seedsim/as_ixp.py
+++ InternetSimulator/src/seedsim/as_ixp.py
@@ -18,14 +18,14 @@ class AS:
         @param net_name The name of the AS internal network.
         @param simulator Reference to the parent simulator object."""
 
-        self.name = name
-        self.asn = asn
-        self.network = net_name   # Use network name
-        self.type = as_type
-        self.simulator = simulator
+        self.__name = name
+        self.__asn = asn
+        self.__network = net_name   # Use network name
+        self.__type = as_type
+        self.__simulator = simulator
         # Set of AS peers, each peer is a pair (peer name, location: ixp name)
-        self.peers = set()
-        self.bgprouters = set()  # Set of BGP routers (name) in this AS
+        self.__peers = set()
+        self.__bgprouters = set()  # Set of BGP routers (name) in this AS
 
     # Add (AS IX) to the peer list: self peers with AS at IX
     def addPeer(self, as_name: str, ix_name: str):
@@ -35,29 +35,29 @@ class AS:
         @param as_name The name of the perr AS.
         @param ix_name The name of the IX to establish this peering."""
 
-        self.peers.add((as_name, ix_name))
+        self.__peers.add((as_name, ix_name))
 
     def printDetails(self):
         """!Print the details to stdout.
 
         @param self The object pointer."""
-        print("Name: {} -- Network: {} -- Type: {}".format(self.name,
-                                                           self.network, self.type))
+        print("Name: {} -- Network: {} -- Type: {}".format(self.__name,
+                                                           self.__network, self.__type))
 
     def getBGPRouters(self):
         """!Get the set of BGP routers.
 
         @param self The object pointer."""
-        return self.bgprouters
+        return self.__bgprouters
 
     def addBGPRouter(self, router_name: str):
         """!Add a new BGP router.
 
         @param self The object pointer.
         @router_name The name of the new router."""
-        self.bgprouters.add(router_name)
+        self.__bgprouters.add(router_name)
 
-    # Future work: we can generate dot files using self.peers
+    # Future work: we can generate dot files using self.__peers
     def generateDotFile(self):
         """!Generate Graphviz dot file for this AS.
 
@@ -80,12 +80,12 @@ class IXP:
         @param net_name The name of the IXP network.
         @param simulator Reference to the parent simulator object."""
 
-        self.name = name
-        self.asn = asn
-        self.net_name = net_name    # Use network name
-        self.simulator = simulator
-        self.bgp_routers = set()    # Routers (names) in this IXP
-        self.type = 'IX'
+        self.__name = name
+        self.__asn = asn
+        self.__net_name = net_name    # Use network name
+        self.__simulator = simulator
+        self.__bgp_routers = set()    # Routers (names) in this IXP
+        self.__type = 'IX'
 
     # Add Router to the list using router name
     def addBGPRouter(self, router_name: str):
@@ -93,11 +93,11 @@ class IXP:
 
         @param self The object pointer.
         @param name The name of the router."""
-        self.bgp_routers.add(router_name)
+        self.__bgp_routers.add(router_name)
 
     def printDetails(self):
         """!Print the details to stdout.
 
         @param self The object pointer."""
-        print("Name: {} -- Network: {} -- Type: {}".format(self.name,
-                                                           self.net_name, self.type))
+        print("Name: {} -- Network: {} -- Type: {}".format(self.__name,
+                                                           self.__net_name, self.__type))
