737d670fd5cb5301e7fa3a4e9882307f7fb5b454
Maho Morichika <nat@magicnat.com>
2020-09-25 01:46:13 -0400

    host defgw

--- InternetSimulator/src/seedsim/layers/Routing.py
+++ InternetSimulator/src/seedsim/layers/Routing.py
@@ -1,16 +1,14 @@
 from .Layer import Layer
-from seedsim.core import Registry
+from seedsim.core import Registry, ScopedRegistry, Node, Interface, Network
 from typing import List
 
 class Routing(Layer):
     """!
     @brief The Routing layer.
 
-    This layer provides routing support for routers. i.e., this layer install
-    BIRD on nodes and allow BGP/OSPF to work.
-
-    This layer will also setup direct protocol for host networks, kernel
-    protocol, etc.
+    This layer provides routing support for routers and hosts. i.e., (1) install
+    BIRD on router nodes and allow BGP/OSPF to work, (2) setup kernel and device
+    protocols, and (3) setup defult routes for host nodes.
     """
 
     __reg: Registry = Registry()
@@ -28,6 +26,25 @@ class Routing(Layer):
                 
             if type == 'rnode':
                 print("===== RoutingLayer: TODO: Bootstrap bird.conf for AS{} router {}".format(scope, name))
+
+            if type == 'hnode':
+                hifaces: List[Interface] = obj.getInterfaces()
+                assert len(hifaces) == 1, 'Host {} in as{} has != 1 interfaces'.format(name, scope)
+                hif = hifaces[0]
+                hnet: Network = hif.getNet()
+                rif: Interface = None
+
+                cur_scope = ScopedRegistry(scope)
+                for router in cur_scope.getByType('rnode'):
+                    if rif != None: break
+                    for riface in router.getInterfaces():
+                        if riface.getNet() == hnet:
+                            rif = riface
+                            break
+                
+                assert rif != None, 'Host {} in as{} in network {}: no router'.format(name, scope, hnet.getName())
+                print("===== RoutingLayer: TODO: Set default route for host {} ({}) to router {}".format(name, hif.getAddress(), rif.getAddress()))
+
 
     def print(self, indent: int) -> str:
         out = ' ' * indent
