d1de790f27c24fc4c0b1f9e95fbb83d4e1ee3053
Maho Morichika <nat@magicnat.com>
2020-09-26 16:59:34 -0400

    basic c/u

--- InternetSimulator/src/seedsim/layers/Ospf.py
+++ InternetSimulator/src/seedsim/layers/Ospf.py
@@ -1,6 +1,7 @@
 from .Layer import Layer
-from seedsim.core import Network
-from typing import List
+from seedsim.core import Network, Registry
+from seedsim.core.enums import NetworkType
+from typing import Set
 
 class Ospf(Layer):
     """!
@@ -11,6 +12,18 @@ class Ospf(Layer):
     created by BaseLayer::createNetwork) OSPF interface. Other interfaces like
     the IX interface will also be added as stub interface.
     """
+
+    __stubs: Set[Network]
+    __masked: Set[Network]
+    __reg = Registry()
+
+    def __init__(self):
+        """!
+        @brief Ospf (OSPF) layer conscrutor.
+        """
+
+        self.__stubs = set()
+        self.__masked = set()
 
     def getName(self) -> str:
         return 'Ospf'
@@ -29,8 +42,10 @@ class Ospf(Layer):
         internal networks as a stub.
 
         @param net Network.
+        @throws AssertionError if network is not local
         """
-        pass
+        assert net.getType() != NetworkType.InternetExchange, 'cannot operator on IX network.'
+        self.__stubs.add(net)
 
     def markStubByName(self, asn: int, netname: str):
         """!
@@ -45,32 +60,34 @@ class Ospf(Layer):
         @param asn ASN to operate on.
         @param netname name of the network.
         """
-        pass
+        self.markStub(self.__reg.get(str(asn), 'net', netname))
 
-    def mask(self, asn: int, net: Network):
+    def mask(self, net: Network):
         """!
         @brief Remove all OSPF interfaces connected to a network.
 
-        By default, all internal networks will be active OSPF interface, and all
-        IX interface will be stub (i.e., passive) OSPF interface. Use this
-        method to mask a network and disable OSPF on connected interface.
+        By default, all internal networks will be active OSPF interface. Use
+        this method to mask a network and disable OSPF on all connected
+        interface.
 
-        @param asn scope of the mask. For example, if you are trying to mask an
-        IX peering LAN for a specific AS.
+        @todo handle IX LAN masking?
+
         @param net network.
+        @throws AssertionError if network is not local.
         """
-        pass
+        assert net.getType() != NetworkType.InternetExchange, 'cannot operator on IX network.'
+        self.__masked.add(net)
 
     def maskByName(self, asn: int, netname: str):
         """!
         @brief Remove all OSPF interfaces connected to a network.
 
-        By default, all internal networks will be active OSPF interface, and all
-        IX interface will be stub (i.e., passive) OSPF interface. Use this
-        method to mask a network and disable OSPF on connected interface.
+        By default, all internal networks will be active OSPF interface. Use
+        this method to mask a network and disable OSPF on all connected
+        interface.
 
         @param asn ASN to operate on.
         @param netname name of the network.
         """
-        pass
+        self.mask(self.__reg.get(str(asn), 'net', netname))
 
