232250b5c99cd1c3e0d09ee84407859ab5951a2b
Maho Morichika <nat@magicnat.com>
2020-11-30 02:21:27 -0500

    license & setup

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name = "seedsim",
+    version = "0.0.1",
+    author = "Honghao Zeng",
+    author_email = "hozeng@syr.edu",
+    description = "SEED Internet Simulator",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/seed-labs/seed-simulator",
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = '>=3.6',
+)
