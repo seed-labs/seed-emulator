2e68f3141a3ec47fdca0384d21a3a2136b51121a
Maho Morichika <nat@magicnat.com>
2020-11-30 02:32:39 -0500

    bump

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 
-with open('README.md', 'r') as readme:
+with open("README.md", "r") as readme:
     long_description = readme.read()
 
 setuptools.setup(
     name = "seedsim",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Honghao Zeng",
     author_email = "hozeng@syr.edu",
     description = "SEED Internet Simulator",
@@ -18,5 +18,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires = '>=3.6',
+    install_requires = [
+        'requests'
+    ],
+    python_requires = '>=3.6'
 )
