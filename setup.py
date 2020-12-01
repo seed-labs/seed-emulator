64c57ddc7fa64f4cce52a7c02216e90e1ff43787
Maho Morichika <nat@magicnat.com>
2020-12-01 02:31:56 -0500

    bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name = "seedsim",
-    version = "0.0.3",
+    version = "0.0.4",
     author = "Honghao Zeng",
     author_email = "hozeng@syr.edu",
     description = "SEED Internet Simulator",
