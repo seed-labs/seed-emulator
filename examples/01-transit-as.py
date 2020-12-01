62e6b3bdd5fa79a9b17f3da6f90c9d265abfc4c2
Maho Morichika <nat@magicnat.com>
2020-12-01 05:31:15 -0500

    fin. transit

--- examples/01-transit-as.py
+++ examples/01-transit-as.py
@@ -48,3 +48,57 @@ r4.joinNetworkByName('net2')
 r4.joinNetworkByName('ix101')
 
 ###############################################################################
+
+as151 = base.createAutonomousSystem(151)
+
+as151_web = as151.createHost('web')
+web.installOn(as151_web)
+
+as151_router = as151.createRouter('router0')
+
+as151_net = as151.createNetwork('net0')
+
+routing.addDirect(as151_net)
+
+as151_web.joinNetwork(as151_net)
+as151_router.joinNetwork(as151_net)
+
+as151_router.joinNetworkByName('ix100')
+
+###############################################################################
+
+as152 = base.createAutonomousSystem(152)
+
+as152_web = as152.createHost('web')
+web.installOn(as152_web)
+
+as152_router = as152.createRouter('router0')
+
+as152_net = as152.createNetwork('net0')
+
+routing.addDirect(as152_net)
+
+as152_web.joinNetwork(as152_net)
+as152_router.joinNetwork(as152_net)
+
+as152_router.joinNetworkByName('ix101')
+
+###############################################################################
+
+ebgp.addPrivatePeering(100, 150, 151)
+ebgp.addPrivatePeering(101, 150, 152)
+
+###############################################################################
+
+rendrer.addLayer(base)
+rendrer.addLayer(routing)
+rendrer.addLayer(ebgp)
+rendrer.addLayer(ibgp)
+rendrer.addLayer(ospf)
+rendrer.addLayer(web)
+
+rendrer.render()
+
+###############################################################################
+
+docker_compiler.compile('./transit-as')
