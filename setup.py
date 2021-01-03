3b30d0fc3edb538b3d831a83ea02b55d5d35cee0
Maho Morichika <nat@magicnat.com>
2021-01-03 10:49:45 -0500

    published as 0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name = "seedsim",
-    version = "0.0.6",
+    version = "0.0.7",
     author = "Honghao Zeng",
     author_email = "hozeng@syr.edu",
     description = "SEED Internet Simulator",
