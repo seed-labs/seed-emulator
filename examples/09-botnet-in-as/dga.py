b7fd8fdf14bc8c73559b1a7a8b8beab4e12d9840
keyi.lky <kli148@syr.edu>
2021-02-16 23:09:26 -0500

    update dga

--- examples/09-botnet-in-as/dga.py
+++ examples/09-botnet-in-as/dga.py
@@ -1,14 +1,22 @@
-def generate_domain(year: int, month: int, day: int) -> str:
-    """Generate a domain name for the given date."""
+def dga() -> list:
+    #Generate 10 domains for the given time.
+    domain_list = []
     domain = ""
+    import math, datetime
+    today = datetime.datetime.utcnow()
+    hour = today.hour
+    day = today.day
+    minute = today.minute
+    minute = int(math.ceil(minute/5))*5
 
     for i in range(16):
-        year = ((year ^ 8 * year) >> 11) ^ ((year & 0xFFFFFFF0) << 17)
-        month = ((month ^ 4 * month) >> 25) ^ 16 * (month & 0xFFFFFFF8)
-        day = ((day ^ (day << 13)) >> 19) ^ ((day & 0xFFFFFFFE) << 12)
-        domain += chr(((year ^ month ^ day) % 25) + 97)
+        day = ((day ^ 8 * day) >> 11) ^ ((day & 0xFFFFFFF0) << 17)
+        hour = ((hour ^ 4 * hour) >> 25) ^ 16 * (hour & 0xFFFFFFF8)
+        minute = ((minute ^ (minute << 13)) >> 19) ^ ((minute & 0xFFFFFFFE) << 12)
+        domain += chr(((day ^ hour ^ minute) % 25) + 97)
+        if i > 6:
+            domain_list.append(domain+ ".com")
 
-    return domain + ".com"
+    return domain_list
 
-
-print(generate_domain(2021, 1,22))
+print(dga())
