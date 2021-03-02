7ae3c87172d7736bcb6aec462a3735800cb2658b
Maho Morichika <nat@magicnat.com>
2021-03-01 21:37:27 -0500

    ospf merger

--- seedsim/mergers/DefaultMergers.py
+++ seedsim/mergers/DefaultMergers.py
@@ -2,7 +2,8 @@ from .DefaultBaseMerger import DefaultBaseMerger
 from .DefaultEbgpMerger import DefaultEbgpMerger
 from .DefaultRoutingMerger import DefaultRoutingMerger
 from .DefaultIbgpMerger import DefaultIbgpMerger
+from .DefaultOspfMerger import DefaultOspfMerger
 
 DEFAULT_MERGERS = [
     DefaultBaseMerger(), DefaultEbgpMerger(), DefaultRoutingMerger(),
-    DefaultIbgpMerger()]
+    DefaultIbgpMerger(), DefaultOspfMerger()]
--- seedsim/mergers/__init__.py
+++ seedsim/mergers/__init__.py
@@ -3,3 +3,4 @@ from .DefaultBaseMerger import DefaultBaseMerger
 from .DefaultEbgpMerger import DefaultEbgpMerger
 from .DefaultRoutingMerger import DefaultRoutingMerger
 from .DefaultIbgpMerger import DefaultIbgpMerger
+from .DefaultOspfMerger import DefaultOspfMerger
