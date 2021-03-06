6c3b77f626a6dbef1b5ee93494323772e3ef90c1
Maho Morichika <nat@magicnat.com>
2021-03-05 20:05:22 -0500

    fix ex11 for new service

--- examples/11-dns-infra-combine/11-dns-infra-dump.py
+++ examples/11-dns-infra-combine/11-dns-infra-dump.py
@@ -4,10 +4,10 @@ from seedsim.core import Simulator
 sim = Simulator()
 dns = DomainNameService()
 
-dns.installByName(150, 'root_server').addZone(dns.getZone('.'))
-dns.installByName(151, 'com_server').addZone(dns.getZone('com.')) 
-dns.installByName(152, 'net_server').addZone(dns.getZone('net.'))
-dns.installByName(153, 'example_com_server').addZone(dns.getZone('example.com.'))
+dns.install('root_server').addZone(dns.getZone('.'))
+dns.install('com_server').addZone(dns.getZone('com.')) 
+dns.install('net_server').addZone(dns.getZone('net.'))
+dns.install('example_com_server').addZone(dns.getZone('example.com.'))
 
 sim.addLayer(dns)
 sim.dump('dns-dump.bin')
