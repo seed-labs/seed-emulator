e08d4e30152ea3a1c769383774b3d23575ec6492
Maho Morichika <nat@magicnat.com>
2021-03-09 09:33:37 -0500

    fix bgp attack comp

--- examples/14-bgp-attacker-component/14-bgp-attacker-component.py
+++ examples/14-bgp-attacker-component/14-bgp-attacker-component.py
@@ -1,7 +1,17 @@
 from seedsim.core import Simulator
-from seedsim.layers import Base, Routing, Ebgp
+from seedsim.layers import Base, Routing, Ebgp, Ospf, Ibgp, PeerRelationship
 from seedsim.components import BgpAttackerComponent
+from seedsim.compiler import Docker
+from seedsim.mergers import DEFAULT_MERGERS
 
+###############################################################################
+# topology:
+#
+# as150 --+
+#          \__ ix100 -- as2 -- ix101 -- as151 
+#          /
+# as666 --+
+# (hijacking as151)
 ###############################################################################
 
 sim = Simulator()
@@ -9,9 +19,83 @@ sim = Simulator()
 base = Base()
 routing = Routing()
 ebgp = Ebgp()
+ospf = Ospf()
+ibgp = Ibgp()
 
 bgp_attack = BgpAttackerComponent()
 
 ###############################################################################
 
-base.createInternetExchange
+base.createInternetExchange(100)
+base.createInternetExchange(101)
+
+###############################################################################
+
+as150 = base.createAutonomousSystem(150)
+
+as150_r0 = as150.createRouter('r0')
+
+as150_n0 = as150.createNetwork('n0')
+
+routing.addDirect(150, 'n0')
+
+as150_r0.joinNetwork('n0')
+as150_r0.joinNetwork('ix100')
+
+###############################################################################
+
+as2 = base.createAutonomousSystem(2)
+
+as2_r0 = as2.createRouter('r0')
+as2_r1 = as2.createRouter('r1')
+
+as2_n0 = as2.createNetwork('n0')
+
+as2_r0.joinNetwork('n0')
+as2_r1.joinNetwork('n0')
+
+as2_r0.joinNetwork('ix100')
+as2_r1.joinNetwork('ix101')
+
+###############################################################################
+
+as151 = base.createAutonomousSystem(151)
+
+as151_r0 = as151.createRouter('r0')
+
+as151_n0 = as151.createNetwork('n0')
+
+routing.addDirect(151, 'n0')
+
+as151_r0.joinNetwork('n0')
+as151_r0.joinNetwork('ix101')
+
+###############################################################################
+
+sim.addLayer(base)
+sim.addLayer(routing)
+sim.addLayer(ibgp)
+sim.addLayer(ebgp)
+sim.addLayer(ospf)
+
+###############################################################################
+
+bgp_attack.addHijackedPrefix(as151_n0.getPrefix())
+bgp_attack.joinInternetExchange('ix100')
+
+hijack_component = bgp_attack.get()
+
+sim_with_attack = sim.merge(bgp_attack.get(), DEFAULT_MERGERS)
+
+###############################################################################
+
+ebgp.addPrivatePeering(100, 2, 150, PeerRelationship.Provider)
+ebgp.addPrivatePeering(101, 2, 151, PeerRelationship.Provider)
+
+# hijacker's session
+ebgp.addPrivatePeering(100, 2, 666, PeerRelationship.Unfiltered)
+
+###############################################################################
+
+sim_with_attack.render()
+sim_with_attack.compile(Docker(), 'bgp-attacker-component')
--- seedsim/components/BgpAttackerComponent.py
+++ seedsim/components/BgpAttackerComponent.py
@@ -1,10 +1,11 @@
 from seedsim.core import Component, Simulator, AutonomousSystem
-from seedsim.layers import Base, Router
+from seedsim.layers import Base, Router, Routing
 
 class BgpAttackerComponent(Component):
 
     __data: Simulator
     __hijacker_as: AutonomousSystem
+    __routing: Routing
     __hijacker: Router
     __counter: int
 
@@ -13,17 +14,23 @@ class BgpAttackerComponent(Component):
         self.__counter = 0
 
         base = Base()
+        self.__routing = Routing()
 
         self.__hijacker_as = base.createAutonomousSystem(666)
         self.__hijacker = self.__hijacker_as.createRouter('hijacker')
 
         self.__data.addLayer(base)
+        self.__data.addLayer(self.__routing)
 
     def get(self) -> Simulator:
         return self.__data
 
     def addHijackedPrefix(self, prefix: str):
-        self.__hijacker.addProtocol('static', 'h{}'.format(self.__counter), '''
-            route {} via blackhole;
-        ''')
+        netname = 'h{}'.format(self.__counter)
+        self.__hijacker_as.createNetwork(netname, prefix)
+        self.__hijacker.joinNetwork(netname)
+        self.__routing.addDirect(666, netname)
         self.__counter += 1
+
+    def joinInternetExchange(self, ix: str):
+        self.__hijacker.joinNetwork(ix)
