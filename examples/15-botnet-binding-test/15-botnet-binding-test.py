40ecc9656d3899c2edda4f8de165f717aa3b262c
keyi.lky <kli148@syr.edu>
2021-03-10 17:01:10 -0500

    add botnet binding test

---
+++ examples/15-botnet-binding-test/15-botnet-binding-test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# __author__ = 'Demon'
+
+from seedsim.core import Simulator, Binding, Filter
+from seedsim.compiler import Docker
+
+
+simA = Simulator()
+simB = Simulator()
+
+simA.load('botnet-component.bin')
+simB.load('base-component.bin')
+
+merged = simB.merge(simA,[])
+
+#Binding C2 Server
+merged.addBinding(Binding("c2_server", filter = Filter(asn = 150)))
+
+
+#Binding BOTS
+merged.addBinding(Binding("bot\w", filter = Filter()))
+
+merged.render()
+
+###############################################################################
+
+merged.compile(Docker(), './botnet-binding')
