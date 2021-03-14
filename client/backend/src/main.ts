c3f170b16b7b015677542145e109a6949970d031
Maho Morichika <nat@magicnat.com>
2021-03-13 20:48:48 -0500

    new static folder

--- client/backend/src/main.ts
+++ client/backend/src/main.ts
@@ -6,7 +6,7 @@ expressWs(app);
 
 import apiV1Router from './api/v1/main';
 
-app.use(express.static('../container-manager-client/public'));
+app.use(express.static('../frontend/public'));
 app.use('/api/v1', apiV1Router);
 
 app.listen(8080, '0.0.0.0');
