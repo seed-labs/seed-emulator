365ba9f02e696eabb4320d7024d1bb743beeabfd
keyi.lky <kli148@syr.edu>
2021-03-16 20:41:35 -0400

    add dns service merger

--- examples/13-dns-binding-test/13-dns-binding-test.py
+++ examples/13-dns-binding-test/13-dns-binding-test.py
@@ -6,37 +6,42 @@
 from seedsim.layers import Base, Routing, Ebgp, Dnssec
 from seedsim.services import DomainNameService, DomainNameCachingService, WebService
 from seedsim.core import Simulator, Binding, Filter, Action
+from seedsim.mergers import DEFAULT_MERGERS
 from seedsim.hooks import ResolvConfHook
 from seedsim.compiler import Docker
 
 
 simA = Simulator()
 simB = Simulator()
+simC = Simulator()
 
 simA.load('dns-component.bin')
 simB.load('base-component.bin')
+simC.load('dns-component-1.bin')
 
-dns: DomainNameService = simA.getLayer('DomainNameService')
-ldns: DomainNameCachingService = simA.getLayer('DomainNameCachingService')
+merged = simA.merge(simB, DEFAULT_MERGERS).merge(simC, DEFAULT_MERGERS)
 
-root_zones = dns.getZoneServerNames('.')
+#Add binding for First Component
+merged.addBinding(Binding('a-root-server', filter = Filter(asn = 150)))
+merged.addBinding(Binding('a-com-server', filter = Filter(asn = 151)))
+merged.addBinding(Binding('a-net-server', filter = Filter(asn = 153)))
+merged.addBinding(Binding('a-gov-server', filter = Filter(asn = 152), action=Action.LAST))
+merged.addBinding(Binding('a-edu-server', filter = Filter(prefix = "10.160.0.0/24")))
+merged.addBinding(Binding('a-cn-server', filter = Filter()))
+merged.addBinding(Binding('ns[1-3]-dnspod.com', filter = Filter(custom = lambda vnode_name, node: node.getAsn() == 154 )))
+merged.addBinding(Binding('local-dns-\w', filter = Filter(asn=152), action=Action.FIRST))
 
-simB.addBinding(Binding('a-root-server', filter = Filter(asn = 150)))
-simB.addBinding(Binding('a-com-server', filter = Filter(asn = 151)))
-simB.addBinding(Binding('a-net-server', filter = Filter(asn = 153)))
-simB.addBinding(Binding('a-gov-server', filter = Filter(asn = 152), action=Action.LAST))
-simB.addBinding(Binding('a-edu-server', filter = Filter(prefix = "10.160.0.0/24")))
-simB.addBinding(Binding('a-cn-server', filter = Filter()))
-simB.addBinding(Binding('ns[1-3]-dnspod.com', filter = Filter(custom = lambda vnode_name, node: node.getAsn() == 154 )))
-simB.addBinding(Binding('local-dns-\w', filter = Filter(asn=152), action=Action.FIRST))
+#Add binding for Second Component
+merged.addBinding(Binding('a-uk-server', filter = Filter()))
+merged.addBinding(Binding('a-com-uk-server', filter = Filter()))
+merged.addBinding(Binding('a-net-uk-server', filter = Filter()))
+merged.addBinding(Binding('godaddy', filter = Filter()))
 
-simB.addLayer(dns)
-simB.addLayer(ldns)
 
-simB.addHook(ResolvConfHook(['10.152.0.71']))
+merged.addHook(ResolvConfHook(['10.152.0.71']))
 
-simB.render()
+merged.render()
 
 ###############################################################################
 
-simB.compile(Docker(), './dns-binding')
+merged.compile(Docker(), './dns-binding')
---
+++ examples/13-dns-binding-test/13-dns-component-1.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# __author__ = 'Demon'
+
+from seedsim.services import DomainNameService, DomainNameCachingService
+from seedsim.core import Simulator
+
+
+sim = Simulator()
+
+dns = DomainNameService()
+ldns = DomainNameCachingService()
+
+#Install UK TLD server
+dns.install('a-uk-server').addZone(dns.getZone('uk.'))
+
+#Install UK sub zones
+dns.install('a-com-uk-server').addZone(dns.getZone('com.uk.'))
+dns.install('a-net-uk-server').addZone(dns.getZone('net.uk.'))
+
+#Add some default record
+com_uk = dns.getZone('company.com.uk.')
+net_uk = dns.getZone('example.net.uk.')
+
+com_uk.addRecord('@ A 1.1.1.1')
+com_uk.addRecord('www A 1.1.1.1')
+
+net_uk.addRecord('@ A 1.1.1.1')
+net_uk.addRecord('www A 7.7.7.7')
+
+#CONFLICT TEST
+# twitter_com = dns.getZone('www.twitter.com.')
+# twitter_com.addRecord('abc A 11.11.11.11')
+
+
+#Host com_uk and net_uk on godaddy server
+dns.install('godaddy').addZone(com_uk)
+dns.install('godaddy').addZone(net_uk)
+#dns.install('godaddy').addZone(twitter_com)
+
+
+sim.addLayer(dns)
+
+sim.dump('dns-component-1.bin')
+
+
+
