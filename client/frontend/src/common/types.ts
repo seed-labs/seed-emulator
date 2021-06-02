eaeb2a32af0f1f70bb62090b2b5f2e5ea9ba810d
Maho Morichika <nat@magicnat.com>
2021-06-02 11:41:43 -0400

    client-front: types

---
+++ client/frontend/src/common/types.ts
@@ -0,0 +1,34 @@
+export interface EmulatorNode {
+    Id: string;
+    NetworkSettings: {
+        Networks: {
+            [name: string]: {
+                NetworkID: string
+            }
+        }
+    
+    };
+    meta: {
+        emulatorInfo: {
+            nets: {
+                name: string,
+                address: string
+            }[],
+            asn: number,
+            name: string,
+            role: string
+        };
+    };
+};
+
+export interface EmulatorNetwork {
+    Id: string;
+    meta: {
+        emulatorInfo: {
+            type: string;
+            scope: string;
+            name: string;
+            prefix: string;
+        }
+    }
+};
