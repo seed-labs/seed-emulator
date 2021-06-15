b475cc4dce0dc31925a276bd0979df1e8ce844ad
Maho Morichika <nat@magicnat.com>
2021-06-15 17:56:17 -0400

    service-merger: comment

--- seedemu/mergers/ServiceMerger.py
+++ seedemu/mergers/ServiceMerger.py
@@ -2,11 +2,31 @@ from seedemu.core.Service import Service
 from seedemu.core import Merger
 
 class ServiceMerger(Merger):
+    """!
+    @brief Merger that handles merging installation targets.
+    """
 
     def _createService(self) -> Service:
+        """!
+        @brief create a new services instance of the service to be merged.
+
+        @returns service instance.
+        """
+
         raise NotImplementedError('_createService not implemented')
 
     def doMerge(self, objectA: Service, objectB: Service) -> Service:
+        """!
+        @brief merge installation targets.
+
+        @param objectA first service instance.
+        @param objectB second service instance.
+
+        @returns merged services.
+        """
+
+        assert objectA.getName() == objectB.getName(), 'cannot merge different services.'
+
         new_service = self._createService()
 
         new_service.setPendingTargets(dict(objectA.getPendingTargets(), **objectB.getPendingTargets()))
