1a41bd73a99a6edcea76b7baa0bfc6142913e16a
Maho Morichika <nat@magicnat.com>
2021-06-15 18:07:02 -0400

    bgp-lg: fix doxygen

--- seedemu/mergers/DefaultBgpLookingGlassServiceMerger.py
+++ seedemu/mergers/DefaultBgpLookingGlassServiceMerger.py
@@ -2,6 +2,12 @@ from .ServiceMerger import ServiceMerger
 from seedemu.services import BgpLookingGlassService
 
 class DefaultBgpLookingGlassServiceMerger(ServiceMerger):
+    """!
+    @brief default BGP looking glass service merger implementation.
+
+    This is the defualt implementation which invokes the default service merger
+    to handler merging installation targets.
+    """
 
     def getTargetType(self) -> str:
         return 'BgpLookingGlassServiceLayer'
