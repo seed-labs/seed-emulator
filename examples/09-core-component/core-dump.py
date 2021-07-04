1ac6442a78566f5fb5c3a55563e340cac635a835
Kevin Du <wedu@syr.edu>
2021-07-04 13:16:33 -0400

    rewrote it

--- examples/09-core-component/core-dump.py
+++ examples/09-core-component/core-dump.py
@@ -1,8 +1,9 @@
 from seedemu.layers import Base, Routing, Ebgp, PeerRelationship, Ibgp, Ospf
 from seedemu.services import WebService
-from seedemu.core import Emulator
+from seedemu.core import Emulator, Binding, Filter
+from seedemu.compiler import Docker
 
-sim = Emulator()
+emu = Emulator()
 
 base = Base()
 routing = Routing()
@@ -17,6 +18,7 @@ base.createInternetExchange(100)
 base.createInternetExchange(101)
 
 ###############################################################################
+# Set up the transit AS (AS150)
 
 as150 = base.createAutonomousSystem(150)
 
@@ -42,11 +44,13 @@ r4.joinNetwork('net2')
 r4.joinNetwork('ix101')
 
 ###############################################################################
+# Create and set up the AS 151
 
 as151 = base.createAutonomousSystem(151)
 
 as151_web = as151.createHost('web')
-web.installByName(151, 'web')
+web.install('web151')
+emu.addBinding(Binding('web151', filter = Filter(nodeName = 'web', asn = 151)))
 
 as151_router = as151.createRouter('router0')
 
@@ -60,11 +64,13 @@ as151_router.joinNetwork('net0')
 as151_router.joinNetwork('ix100')
 
 ###############################################################################
+# Create and set up the AS 152
 
 as152 = base.createAutonomousSystem(152)
 
 as152_web = as152.createHost('web')
-web.installByName(152, 'web')
+web.install('web152')
+emu.addBinding(Binding('web152', filter = Filter(nodeName = 'web', asn = 152)))
 
 as152_router = as152.createRouter('router0')
 
@@ -78,17 +84,18 @@ as152_router.joinNetwork('net0')
 as152_router.joinNetwork('ix101')
 
 ###############################################################################
+# Add BGP peering
 
 ebgp.addPrivatePeering(100, 150, 151, abRelationship = PeerRelationship.Provider)
 ebgp.addPrivatePeering(101, 150, 152, abRelationship = PeerRelationship.Provider)
 
 ###############################################################################
 
-sim.addLayer(base)
-sim.addLayer(routing)
-sim.addLayer(ebgp)
-sim.addLayer(ibgp)
-sim.addLayer(ospf)
-sim.addLayer(web)
+emu.addLayer(base)
+emu.addLayer(routing)
+emu.addLayer(ebgp)
+emu.addLayer(ibgp)
+emu.addLayer(ospf)
+emu.addLayer(web)
 
-sim.dump('10-dump.bin')
+emu.dump('component-dump.bin')
