018fd9874cd8b01cff4527bb2f6905f7cac988ee
Maho Morichika <nat@magicnat.com>
2021-07-09 17:29:09 -0400

    layers: default export (rw)router

--- seedemu/layers/__init__.py
+++ seedemu/layers/__init__.py
@@ -1,8 +1,7 @@
 from .Base import Base
-from .Routing import Routing, Router
+from .Routing import Routing, Router, RealWorldRouter
 from .Ebgp import Ebgp, PeerRelationship
 from .Ospf import Ospf
 from .Ibgp import Ibgp
-from .Reality import Reality
 from .Dnssec import Dnssec
 from .Mpls import Mpls
