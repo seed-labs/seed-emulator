92e8c58b136ff2ee10234388504299b2f8ffe3b0
Kevin Du <wedu@syr.edu>
2021-07-12 21:57:25 -0400

    clean up the code

--- examples/05-real-world/05-real-world.py
+++ examples/05-real-world/05-real-world.py
@@ -4,18 +4,14 @@ from seedemu.core import Emulator, Binding, Filter
 from seedemu.raps import OpenVpnRemoteAccessProvider
 from seedemu.compiler import Docker
 
-emu = Emulator()
-
-base = Base()
+emu     = Emulator()
+base    = Base()
 routing = Routing()
-ebgp = Ebgp()
-ibgp = Ibgp()
-ospf = Ospf()
-web = WebService()
-
-###############################################################################
-
-ovpn = OpenVpnRemoteAccessProvider()
+ebgp    = Ebgp()
+ibgp    = Ibgp()
+ospf    = Ospf()
+web     = WebService()
+ovpn    = OpenVpnRemoteAccessProvider()
 
 ###############################################################################
 
@@ -23,6 +19,7 @@ base.createInternetExchange(100)
 base.createInternetExchange(101)
 
 ###############################################################################
+# Create AS-150 with 3 internet networks
 
 as150 = base.createAutonomousSystem(150)
 
@@ -30,62 +27,40 @@ as150.createNetwork('net0')
 as150.createNetwork('net1')
 as150.createNetwork('net2')
 
-r1 = as150.createRouter('r1')
-r2 = as150.createRouter('r2')
-r3 = as150.createRouter('r3')
-r4 = as150.createRouter('r4')
+# Create 4 routers: r1 and r4 are BGP routers (connected to internet exchange) 
+as150.createRouter('r1').joinNetwork('ix100').joinNetwork('net0')
+as150.createRouter('r2').joinNetwork('net0').joinNetwork('net1')
+as150.createRouter('r3').joinNetwork('net1').joinNetwork('net2')
+as150.createRouter('r4').joinNetwork('net2').joinNetwork('ix101')
 
-r1.joinNetwork('ix100')
-r1.joinNetwork('net0')
-
-r2.joinNetwork('net0')
-r2.joinNetwork('net1')
-
-r3.joinNetwork('net1')
-r3.joinNetwork('net2')
-
-r4.joinNetwork('net2')
-r4.joinNetwork('ix101')
 
 ###############################################################################
-# Create AS151
+# Create AS-151
 
 as151 = base.createAutonomousSystem(151)
-as151_net0 = as151.createNetwork('net0')
 
+# Create a network and enable the access from machines outside of the emulator
+as151.createNetwork('net0').enableRemoteAccess(ovpn)
 
+# Create a host and a router
 as151.createHost('web').joinNetwork('net0')
+as151.createRouter('router0').joinNetwork('net0').joinNetwork('ix100')
 
-as151_router = as151.createRouter('router0')
-as151_router.joinNetwork('net0')
-as151_router.joinNetwork('ix100')
-
-# Enable the access from machines outside of the emulator
-as151_net0.enableRemoteAccess(ovpn)
 
 ###############################################################################
-# Create AS152
+# Create AS-152
 
 as152 = base.createAutonomousSystem(152)
-as152_net0 = as152.createNetwork('net0')
-
-
+as152.createNetwork('net0').enableRemoteAccess(ovpn)
 as152.createHost('web').joinNetwork('net0')
-
-as152_router = as152.createRouter('router0')
-as152_router.joinNetwork('net0')
-as152_router.joinNetwork('ix101')
-
-# Enable the access from machines outside of the emulator
-as152_net0.enableRemoteAccess(ovpn)
+as152.createRouter('router0').joinNetwork('net0').joinNetwork('ix101')
 
 
 ###############################################################################
 # Create a real-world AS
 
 as11872 = base.createAutonomousSystem(11872)
-as11872_router = as11872.createRealWorldRouter('rw')
-as11872_router.joinNetwork('ix101', '10.101.0.118')
+as11872.createRealWorldRouter('rw').joinNetwork('ix101', '10.101.0.118')
 
 ###############################################################################
 # BGP peering 
@@ -95,11 +70,10 @@ ebgp.addPrivatePeering(101, 150, 152, abRelationship = PeerRelationship.Provider
 ebgp.addPrivatePeering(101, 150, 11872, abRelationship = PeerRelationship.Unfiltered)
 
 ###############################################################################
-# Create two virtual nodes and bind them to physical nodes
+# Create two virtual nodes that run web services, bind them to physical nodes
 
 web.install('web1')
 web.install('web2')
-
 emu.addBinding(Binding('web1', filter = Filter(asn = 151, nodeName = 'web')))
 emu.addBinding(Binding('web2', filter = Filter(asn = 152, nodeName = 'web')))
 
