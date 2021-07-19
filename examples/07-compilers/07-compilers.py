1a2078360730f30b25f67fd6a974a9532df99823
Kevin Du <wedu@syr.edu>
2021-07-19 16:43:29 -0400

    use pre-built component

--- examples/07-compilers/07-compilers.py
+++ examples/07-compilers/07-compilers.py
@@ -4,107 +4,28 @@ from seedemu.services import WebService
 from seedemu.core import Emulator, Binding, Filter
 from os import mkdir
 
+###############################################################################
+# Load the pre-built component from example 01-transit-as
+# Need to go to 01-transit-as and generate the component first
+
 emu = Emulator()
-
-base = Base()
-routing = Routing()
-ebgp = Ebgp()
-mpls = Mpls()
-
-web = WebService()
+emu.load('../01-transit-as/base-component.bin')
 
 ###############################################################################
-
-base.createInternetExchange(100)
-base.createInternetExchange(101)
-
-###############################################################################
-
-as150 = base.createAutonomousSystem(150)
-
-as150.createNetwork('net0')
-as150.createNetwork('net1')
-as150.createNetwork('net2')
-
-r1 = as150.createRouter('r1')
-r2 = as150.createRouter('r2')
-r3 = as150.createRouter('r3')
-r4 = as150.createRouter('r4')
-
-r1.joinNetwork('ix100')
-r1.joinNetwork('net0')
-
-r2.joinNetwork('net0')
-r2.joinNetwork('net1')
-
-r3.joinNetwork('net1')
-r3.joinNetwork('net2')
-
-r4.joinNetwork('net2')
-r4.joinNetwork('ix101')
-
-mpls.enableOn(150)
-
-###############################################################################
-
-as151 = base.createAutonomousSystem(151)
-
-as151_web = as151.createHost('web')
-
-web.install('web151')
-emu.addBinding(Binding('web151', filter = Filter(asn = 151, nodeName = 'web')))
-
-as151_router = as151.createRouter('router0')
-
-as151_net = as151.createNetwork('net0')
-
-
-
-as151_web.joinNetwork('net0')
-as151_router.joinNetwork('net0')
-
-as151_router.joinNetwork('ix100')
-
-###############################################################################
-
-as152 = base.createAutonomousSystem(152)
-
-as152_web = as152.createHost('web')
-
-web.install('web152')
-emu.addBinding(Binding('web152', filter = Filter(asn = 152, nodeName = 'web')))
-
-as152_router = as152.createRouter('router0')
-
-as152_net = as152.createNetwork('net0')
-
-
-
-as152_web.joinNetwork('net0')
-as152_router.joinNetwork('net0')
-
-as152_router.joinNetwork('ix101')
-
-###############################################################################
-
-ebgp.addPrivatePeering(100, 150, 151, abRelationship = PeerRelationship.Provider)
-ebgp.addPrivatePeering(101, 150, 152, abRelationship = PeerRelationship.Provider)
-
-###############################################################################
-
-emu.addLayer(base)
-emu.addLayer(routing)
-emu.addLayer(ebgp)
-emu.addLayer(mpls)
-emu.addLayer(web)
+# Render the emulation
 
 emu.render()
 
+
 ###############################################################################
+# The Registry keeps track of every important objects created in the 
+# emulation (nodes, layers, networks, files, etc.)
+# We can print out these objects 
 
 print(emu.getRegistry())
 
 ###############################################################################
+# Compile the emulation
 
 mkdir('./output')
 emu.compile(Docker(), './output/regular-docker')
