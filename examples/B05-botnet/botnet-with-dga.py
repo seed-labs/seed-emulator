46d2010bea00e5c940607f8d2248f0f333bf2a57
Maho Morichika <nat@magicnat.com>
2021-07-24 22:31:40 -0400

    botnet-dga: cleanup

--- examples/B05-botnet/botnet-with-dga.py
+++ examples/B05-botnet/botnet-with-dga.py
@@ -7,67 +7,60 @@ from seedemu.compiler import Docker
 
 emu = Emulator()
 
+# load the pre-built component
 emu.load('../B00-mini-internet/base-component.bin')
 
+# create services for botnet controller and client
 bot = BotnetService()
-bot_client = BotnetClientService()
+botClient = BotnetClientService()
+
+# create services for DNS, so we can use DGA
 dns = DomainNameService()
-
-###########################################
-
-c2_server_ip = "10.150.0.71"
-
-###########################################
-
-dns.install('root').addZone('.')
-dns.install('com').addZone('com.')
-dns.install('attacker').addZone('attacker.com.')
-
-###########################################
-
-dns.getZone('attacker.com').addRecord('* A {}'.format(c2_server_ip))
-
-###########################################
-
-emu.addBinding(Binding('root', filter=Filter(asn=171)))
-emu.addBinding(Binding('com', filter=Filter(asn=161)))
-emu.addBinding(Binding('attacker', filter=Filter(asn=162)))
-
-###########################################
-
 ldns = DomainNameCachingService()
-ldns.install('global-dns')
-emu.addBinding(Binding('global-dns', filter = Filter(nodeName="gdns", ip = '10.153.0.53'), action = Action.NEW))
 
-###########################################
+# pick an IP for botnet controller
+controllerIp = '10.150.0.71'
 
-# not really how it works in real-world. in real world, one would use TLD.
+# make a script that output candidate domain names.
+# not really how it works in real-world. in real world, one would probably use TLD.
 DGA_SCRIPT = '''\
 #!/bin/bash
 for i in {1..10}; do echo "$RANDOM.attacker.com:446"; done
 '''
 
-##########################################
-base_layer = emu.getLayer('Base')
-base_layer.setNameServers(['10.153.0.53'])
+# points *.attacker.com to botnet controller - our "DGA" generates xxxx.attacker.com.
+dns.getZone('attacker.com').addRecord('* A {}'.format(controllerIp))
 
-x = base_layer.getAutonomousSystem(150)
-hosts = x.getHosts()
+# build a mini dns infrastructure: step 1: create zones
+dns.install('root').addZone('.')
+dns.install('com').addZone('com.')
+dns.install('attacker').addZone('attacker.com.')
 
-bot.install("c2_server")
-emu.addBinding(Binding("c2_server", filter = Filter(ip = c2_server_ip, allowBound=True)))
+# mini dns infra: step 2: bind zone servers to physical nodes
+emu.addBinding(Binding('root', filter = Filter(asn = 171)))
+emu.addBinding(Binding('com', filter = Filter(asn = 161)))
+emu.addBinding(Binding('attacker', filter = Filter(asn = 162)))
 
-for asn in [151,152,153,154,160]:
-    vname = "bot" + str(asn)
-    asn_base = base_layer.getAutonomousSystem(asn)
-    c = bot_client.install(vname)
-    c.setDga(DGA_SCRIPT)
-    emu.addBinding(Binding(vname, filter = Filter(asn = asn, nodeName=asn_base.getHosts()[0], allowBound=True)))
+# mini dns infra: step 3: create a recursive server
+ldns.install('global-dns')
+emu.addBinding(Binding('global-dns', filter = Filter(nodeName="gdns", ip = '10.153.0.53'), action = Action.NEW))
 
+# mini dns infra: step 3: make everyone use the said recursive server
+base = emu.getLayer('Base')
+base.setNameServers(['10.153.0.53'])
 
+# create and bind bot controller
+bot.install('bot_controller')
+emu.addBinding(Binding('bot_controller', filter = Filter(ip = controllerIp, allowBound = True)))
+
+# create and bind bot clients
+for asn in [151, 152, 153, 154, 160]:
+    vname = 'bot{}'.formt(asn)
+    botClient.install(vname).setDga(DGA_SCRIPT)
+    emu.addBinding(Binding(vname, filter = Filter(asn = asn, allowBound = True)))
 
 emu.addLayer(bot)
-emu.addLayer(bot_client)
+emu.addLayer(botClient)
 emu.addLayer(dns)
 emu.addLayer(ldns)
 emu.render()
