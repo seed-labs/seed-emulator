a7b6b8a99f4fa03962d469839dc52af7a477fc22
Kevin Du <wedu@syr.edu>
2021-07-30 13:58:21 -0400

    minor changes

--- examples/B05-botnet/botnet-basic.py
+++ examples/B05-botnet/botnet-basic.py
@@ -13,12 +13,9 @@ emu.load('../B00-mini-internet/base-component.bin')
 ###############################################################################
 # Build a botnet
 
-# Create services for botnet controller and client
+# Create two service layers
 bot = BotnetService()
 botClient = BotnetClientService()
-
-# Pick an IP for botnet controller
-controllerIp = '10.150.0.66'
 
 # Create a virtual node for bot controller,
 # and add a file to this node
@@ -26,16 +23,16 @@ f = open("./ddos.py", "r")
 bot.install('bot_controller').addFile(f.read(), "/tmp/ddos.py")
 
 # Bind the virtual node to a physical node (create a new one)
-emu.addBinding(Binding('bot_controller', filter = Filter(ip = controllerIp), action = Action.NEW))
+emu.addBinding(Binding('bot_controller', 
+               filter = Filter(ip='10.150.0.66'), action=Action.NEW))
 
 for asn in [151, 152, 153, 154]:
     vname = 'bot{}'.format(asn)
 
-    # Create a virtual node for each bot client
+    # Create a virtual node for each bot client,
+    # bind the virtual node to a physical node (new)
     botClient.install(vname).setServer('bot_controller')
-
-    # Bind the virtual node to a physical node (new)
-    emu.addBinding(Binding(vname, filter = Filter(asn = asn), action = Action.NEW))
+    emu.addBinding(Binding(vname, filter = Filter(asn=asn), action=Action.NEW))
 
 ###############################################################################
 
