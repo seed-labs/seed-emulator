265ee425ef3bc77d06870498aa2c3ea10e5783d9
Kevin Du <wedu@syr.edu>
2021-08-04 17:44:51 -0400

    use python3 instead of python

--- examples/19-SmartContract/19-eth-private-network.py
+++ examples/19-SmartContract/19-eth-private-network.py
@@ -1,10 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 # __author__ = 'Demon'
 
 from seedemu.core import Emulator, Binding, Filter
 from seedemu.compiler import Docker
-from seedemu.services import EthereumService
+
+# Use the local copy for now (we made changes)
+from EthereumService import EthereumService
+#from seedemu.services import EthereumService
+
 from EthereumConsoleManager import EthereumConsoleManager
 
 emu = Emulator()
@@ -19,12 +23,10 @@ e2 = eth.install("eth2")
 e3 = eth.install("eth3")
 e4 = eth.install("eth4")
 
-# optionally, set boot nodes.
-e1.setBootNode(True)
-e2.setBootNode(True)
 
-# optionally, set boot node http server port
-e1.setBootNodeHttpPort(8081)
+# Set boot nodes and http server port
+e1.setBootNode(True).setBootNodeHttpPort(8081)
+e2.setBootNode(True)
 
 # add bindings
 emu.addBinding(Binding('eth1', filter = Filter(asn = 150)))
@@ -36,13 +38,17 @@ emu.addLayer(eth)
 emu.render()
 
 #Generate and deploy Smart Contract on node eth1
-esm.startMinerInNode(e1,eth)
-esm.deploySmartContractOn(e1, eth, "./examples/19-SmartContract/SmartContract/contract.bin", "./examples/19-SmartContract/SmartContract/contract.abi")
+esm.startMinerInNode(e1, eth)
+esm.startMinerInNode(e2, eth)
+esm.startMinerInNode(e3, eth)
+esm.startMinerInNode(e4, eth)
+esm.deploySmartContractOn(e1, eth, "./SmartContract/contract.bin", "./SmartContract/contract.abi")
 esm.createNewAccountInNode(e2, eth)
 
-emu.getBindingFor('eth1').setDisplayName('ethereumNode-1')
-emu.getBindingFor('eth2').setDisplayName('ethereumNode-2')
-emu.getBindingFor('eth3').setDisplayName('ethereumNode-3')
-emu.getBindingFor('eth4').setDisplayName('ethereumNode-4')
+# Customization 
+emu.getBindingFor('eth1').setDisplayName('Ethereum-1')
+emu.getBindingFor('eth2').setDisplayName('Ethereum-2')
+emu.getBindingFor('eth3').setDisplayName('Ethereum-3')
+emu.getBindingFor('eth4').setDisplayName('Ethereum-4')
 
 emu.compile(Docker(), './output')
