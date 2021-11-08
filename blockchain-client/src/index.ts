288a8fb75a47db6be0c742c37a2e99bf848ba36c
Rawi Sader <rawinsader@gmail.com>
2021-11-08 16:23:09 -0500

    refactor: made use of routes for better modularization

--- blockchain-client/src/index.ts
+++ blockchain-client/src/index.ts
@@ -1,16 +1,13 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
-const Docker = require('dockerode');
 const path = require('path');
 const bodyParser = require('body-parser');
-const DockerOdeWrapper = require('./DockerOdeWrapper.ts');
-const apis = require('./apis.ts')
-const getCommand = require('./commands.ts')
-
-const docker = new Docker()
 
 const app = express();
 const port = process.env.PORT;
+
+const indexRouter = require('./routes/index.ts');
+const smartContractRouter = require('./routes/smartcontracts.ts');
 
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
@@ -19,32 +16,9 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-app.get('/', async (req, res) => {
-	try {
-		const containers = await DockerOdeWrapper.docker.listContainers(docker)
-		res.render('home', {
-			port,
-			apis,
-			containers: containers.filter(container => container.Names[0].includes('Ethereum')) 
-		})
-	} catch(e) {
-		console.log(e)
-	}
-})
+app.use('/', indexRouter);
+app.use('/smartcontracts', smartContractRouter);
 
-app.post('/', async (req, res) => {
-	try {
-		const {containerId, command, params} = req.body;
-		const container = await DockerOdeWrapper.docker.getContainer(docker, containerId)
-		const output = await DockerOdeWrapper.container.exec(docker, container, getCommand(command, params))	
-		res.send(JSON.stringify({
-			command,
-			output
-		}))
-	} catch(e) {
-		console.log(e)
-	}
-})
 
 app.listen(port, () => {
 	console.log(`server started at http://localhost:${port}`)
