6605e27a41e58ba4c36ec2183bd93c304d1087db
Rawi Sader <rawinsader@gmail.com>
2021-11-08 16:30:29 -0500

    fix: made use of helper function to properly transform the stream, checked docker-modem npm module

--- blockchain-client/src/DockerOdeWrapper.ts
+++ blockchain-client/src/DockerOdeWrapper.ts
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const helpers = require('./common/helpers.ts')
 
 const DockerOdeWrapper = {
 	docker: {
@@ -29,15 +30,16 @@ const DockerOdeWrapper = {
 							reject(err);
 						} else {
 							console.log(`Command \"${command}\" executed successfully`);
-							//option 1
-							//dockerInstance.modem.demuxStream(stream, process.stdout, process.stderr);
-							//option 2
-							//stream.setEncoding('utf8');
-							//stream.pipe(process.stdout);
-							const chunks = []
-							stream.on('data', chunk => chunks.push(Buffer.from(chunk)))
-							stream.on('error', err => reject(err))
-							stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')))
+							return helpers.demuxStream(stream)
+								.then((output) => {	
+									console.log('Exec output: ', output)
+									if(output.error) {
+										return reject(output.data)
+									} else {
+										return resolve(output.data)
+									}
+								})
+
 						}
 					})
 				})
