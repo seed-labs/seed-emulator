f0bf794b4113cb8397d1a6de9fe08da9a9c9afae
Rawi Sader <rawinsader@gmail.com>
2021-11-18 22:41:45 -0500

    feat: automatically running the ethereum client when building this example

--- examples/B08-Remix-Connection/blockchain.py
+++ examples/B08-Remix-Connection/blockchain.py
@@ -47,5 +47,5 @@ emu.render()
 
 # If output directory exists and override is set to false, we call exit(1)
 # updateOutputdirectory will not be called
-emu.compile(Docker(ethClientEnabled=False), output)
+emu.compile(Docker(ethClientEnabled=True), output)
 updateEthStates()
