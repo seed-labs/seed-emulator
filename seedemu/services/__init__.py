a377d0fcc7ec1751decd8cb072ccafdd7ce4542a
Junle Liang <fatluangincn@outlook.com>
2022-03-28 14:12:48 -0400

    update __init__.py

--- seedemu/services/__init__.py
+++ seedemu/services/__init__.py
@@ -3,7 +3,7 @@ from .BotnetService import BotnetClientService, BotnetClientServer, BotnetServic
 from .DomainRegistrarService import DomainRegistrarService, DomainRegistrarServer
 from .DomainNameService import DomainNameServer, DomainNameService, Zone
 from .TorService import TorService, TorServer, TorNodeType
-from .EthereumService import EthereumService, EthereumServer, SmartContract
+from .EthereumService import EthereumService, EthereumServer, SmartContract, ConsensusMechanism
 from .DomainNameCachingService import DomainNameCachingServer, DomainNameCachingService
 from .CymruIpOrigin import CymruIpOriginService, CymruIpOriginServer
 from .ReverseDomainNameService import ReverseDomainNameService, ReverseDomainNameServer
