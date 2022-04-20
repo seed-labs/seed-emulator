88697c53e81a3e93459f234a67bf0847b1ee422e
RawiSader <rawinsader@gmail.com>
2022-04-20 14:11:05 -0400

    merged examples 25 and 27, example 25 now has the manual mode + multiple consensus

--- examples/not-ready-examples/25-proof-of-authority-manual/component-blockchain.py
+++ examples/not-ready-examples/25-proof-of-authority-manual/component-blockchain.py
@@ -16,12 +16,12 @@ eth.setBaseConsensusMechanism(ConsensusMechanism.POA)
 
 # Create Ethereum nodes (nodes in this layer are virtual)
 start=1
-end=7
+end=15
 sealers=[]
 bootnodes=[]
 hport=8544
-#cport=8545 #will be http -> remix
-cport=8546 #will be ws -> visualization and subscriptions
+#cport=8545 remix
+cport=8546 # visualization
 
 # Currently the minimum amount to have to be a validator in proof of stake
 balance = 32 * pow(10, 18)
@@ -32,22 +32,28 @@ for i in range(start, end):
     if i%3 == 0:
         e.setBootNode(True)
         bootnodes.append(i)
-        emu.getVirtualNode('eth{}'.format(i)).setDisplayName('Ethereum-{}-{}'.format("bootnode",i))
     else:
         e.createPrefundedAccounts(balance, 1)
         e.unlockAccounts().startMiner() 
         sealers.append(i)
-        emu.getVirtualNode('eth{}'.format(i)).setDisplayName('Ethereum-{}-{}'.format("miner",i))
-        #emu.getVirtualNode('eth{}'.format(i)).setDisplayName('Ethereum--{}-{}-{}'.format(eth.getBaseConsensusMechanism(),"miner",i))         
     
     e.enableExternalConnection() # not recommended for sealers in production mode
-    emu.getVirtualNode('eth{}'.format(i)).addPortForwarding(hport, cport)
-    #emu.getVirtualNode('eth{}'.format(i)).setDisplayName('Ethereum-{}'.format(i)).addPortForwarding(hport, cport)
+    emu.getVirtualNode('eth{}'.format(i)).setDisplayName('Ethereum-{}'.format(i)).addPortForwarding(hport, cport)
     hport = hport + 1
 
 print("There are {} sealers and {} bootnodes".format(len(sealers), len(bootnodes)))
 print("Sealers {}".format(sealers))
 print("Bootnodes {}".format(bootnodes))
+
+start = end
+end = start + 1
+for i in range(start, end):
+    e = eth.install("eth{}".format(i))
+    e.setConsensusMechanism(ConsensusMechanism.POW)
+    e.unlockAccounts().startMiner()
+    emu.getVirtualNode("eth{}".format(i)).setDisplayName('Ethereum-{}'.format(i))
+
+print("Created {} nodes that use PoW consensus mechanism".format(end - start))
 
 # Add the layer and save the component to a file
 emu.addLayer(eth)
