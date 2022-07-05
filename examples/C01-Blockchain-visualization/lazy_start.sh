efac0e44667c0326da2d0a8bc0c5c3313b32f741
wonkr <kyungrok.won@gmail.com>
2022-07-05 11:42:29 -0400

    clean : archive obsolete folders to the private repostiory and remove files

--- examples/C01-Blockchain-visualization/lazy_start.sh
+++
@@ -1,1 +0,0 @@
- (ls | grep -Ev '.yml$|^dummies$|^eth-states|^lazy_start' | xargs -n10 -exec docker-compose up -d) > /dev/pts/0
--- examples/not-ready-examples/C04-Proof-of-authority/component-blockchain.py
+++ examples/not-ready-examples/C04-Proof-of-authority/component-blockchain.py
@@ -10,11 +10,13 @@ emu = Emulator()
 # manual: requires you to trigger the /tmp/run.sh bash files in each container to lunch the ethereum nodes
 # so the blockchain data will be preserved when containers are deleted.
 # Note: right now we need to manually create the folder for each node (see README.md). 
-eth = EthereumService(saveState = True)
+eth = EthereumService(saveState = True, manual=True)
+
+eth.setBaseConsensusMechanism(ConsensusMechanism.POA)
 
 # Create Ethereum nodes (nodes in this layer are virtual)
 start=1
-end=6
+end=15
 sealers=[]
 bootnodes=[]
 hport=8544
@@ -26,16 +28,15 @@ balance = 32 * pow(10, 18)
 
 # Setting a third of nodes as bootnodes
 for i in range(start, end):
-    e:EthereumServer = eth.install("eth{}".format(i))
-    e.setConsensusMechanism(ConsensusMechanism.POA)
-    if i == 1:
+    e = eth.install("eth{}".format(i))
+    if i%3 == 0:
         e.setBootNode(True)
         bootnodes.append(i)
+    else:
+        e.createPrefundedAccounts(balance, 1)
+        e.unlockAccounts().startMiner() 
+        sealers.append(i)
     
-    e.createAccounts(1, balance)
-    e.unlockAccounts().startMiner() 
-    sealers.append(i)
-
     e.enableExternalConnection() # not recommended for sealers in production mode
     emu.getVirtualNode('eth{}'.format(i)).setDisplayName('Ethereum-{}-poa'.format(i)).addPortForwarding(hport, cport)
     hport = hport + 1
@@ -45,19 +46,13 @@ print("Sealers {}".format(sealers))
 print("Bootnodes {}".format(bootnodes))
 
 start = end
-end = start + 5
+end = start + 1
 for i in range(start, end):
     e = eth.install("eth{}".format(i))
     e.setConsensusMechanism(ConsensusMechanism.POW)
-    if i == 6:
-        e.setBootNode(True)
-    
-    e.createAccounts(1, balance)
     e.unlockAccounts().startMiner()
-    
     e.enableExternalConnection()
     emu.getVirtualNode("eth{}".format(i)).setDisplayName('Ethereum-{}-pow'.format(i)).addPortForwarding(hport, cport)
-    hport = hport + 1
 
 print("Created {} nodes that use PoW consensus mechanism".format(end - start))
 
