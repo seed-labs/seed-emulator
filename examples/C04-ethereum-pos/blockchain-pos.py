f472799f167756466bfad7eb4287af809199de63
wonkr <kyungrok.won@gmail.com>
2023-01-26 17:43:45 -0500

    update pos example

--- examples/C04-ethereum-pos/blockchain-pos.py
+++ examples/C04-ethereum-pos/blockchain-pos.py
@@ -6,7 +6,7 @@ from seedemu import *
 # Create Emulator Base with 10 Stub AS (150-154, 160-164) using Makers utility method.
 # hosts_per_stub_as=3 : create 3 hosts per one stub AS.
 # It will create hosts(physical node) named `host_{}`.format(counter), counter starts from 0. 
-hosts_per_stub_as = 3
+hosts_per_stub_as = 2
 emu = Makers.makeEmulatorBaseWith10StubASAndHosts(hosts_per_stub_as = hosts_per_stub_as)
 
 # Create the Ethereum layer
@@ -19,7 +19,7 @@ eth = EthereumService()
 blockchain = eth.createBlockchain(chainName="pos", consensus=ConsensusMechanism.POS)
 
 # set `terminal_total_difficulty`, which is the value to designate when the Merge is happen.
-blockchain.setTerminalTotalDifficulty(20)
+blockchain.setTerminalTotalDifficulty(40)
 
 asns = [150, 151, 152, 153, 154, 160, 161, 162, 163, 164]
 
@@ -51,17 +51,17 @@ for asn in asns:
         # Validator is added by deposit 32 Ethereum and is activated in realtime after the Merge.
         # isManual=True : deposit 32 Ethereum by manual. 
         #                 Other than deposit part, create validator key and running a validator node is done by codes.  
-        if asn in [152]:
+        if asn in [151]:
             if id == 0:
                 e.enablePOSValidatorAtRunning()
             if id == 1:
                 e.enablePOSValidatorAtRunning(is_manual=True)
-
+        
         # Set hosts in asn 152, 153, 154, and 160 as validator node.
         # These validators are activated by default from genesis status.
         # Before the Merge, when the consensus in this blockchain is still POA, 
         # these hosts will be the signer nodes.
-        if asn in [151,153,154,160,161,162,163,164]:
+        if asn in [152,153,154,160,161,162,163,164]:
             e.enablePOSValidatorAtGenesis()
             e.startMiner()
 
