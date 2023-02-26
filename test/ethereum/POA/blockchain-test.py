45637ea11fd86c7977cc0625a74e173141c11ed3
wonkr <kyungrok.won@gmail.com>
2023-02-25 20:38:22 -0500

    add POA Test

---
+++ test/ethereum/POA/blockchain-test.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import unittest as ut
+from SEEDBlockchain import Wallet
+from web3 import Web3
+from seedemu import *
+import os
+import getopt
+import sys
+import time
+
+class MultipleChainsTestCase(ut.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        os.system("/bin/bash ./emulator-code/run.sh 2> /dev/null &")
+
+        cls.wallet1 = Wallet(chain_id=1337)
+        for name in ['Alice', 'Bob', 'Charlie', 'David', 'Eve']:
+            cls.wallet1.createAccount(name)
+
+        return super().setUpClass()
+        
+    @classmethod
+    def tearDownClass(cls) -> None:
+        '''
+        A classmethod to destruct the some thing after this test case is finished.
+        For this test case, it will down the containers and remove the networks of this test case
+        '''
+        os.system("/bin/bash ./emulator-code/down.sh 2> /dev/null")
+        
+        return super().tearDownClass()
+
+    def test_poa_chain_connection(self):
+        url = 'http://10.150.0.71:8545'
+
+        i = 1
+        current_time = time.time()
+        while True:
+            printLog("\n==========Trial {}==========".format(i))
+            if time.time() - current_time > 600:
+                printLog("TimeExhausted: 600 sec")
+            try:
+                self.wallet1.connectToBlockchain(url, isPOA=True)
+                printLog("Connection Succeed: ", url)
+                break
+            except Exception as e:
+                printLog(e)
+                time.sleep(20)
+                i += 1
+        self.assertTrue(self.wallet1._web3.isConnected())
+
+    def test_poa_chain_id(self):
+        self.assertEqual(self.wallet1._web3.eth.chain_id, 1337)
+    
+    def test_poa_send_transaction(self):
+        time.sleep(10)
+        recipient = self.wallet1.getAccountAddressByName('Alice')
+        txhash = self.wallet1.sendTransaction(recipient, 0.1, sender_name='Eve', wait=True, verbose=False)
+        self.assertTrue(self.wallet1.getTransactionReceipt(txhash)["status"], 1)
+
+    def test_poa_chain_consensus(self):
+        config = dict(self.wallet1._web3.geth.admin.nodeInfo().protocols.eth.config)
+        self.assertTrue("clique" in config.keys())
+
+    def test_poa_peer_counts(self):
+        peer_counts = len(self.wallet1._web3.geth.admin.peers())
+        self.assertEqual(peer_counts, 2)
+
+    def test_import_account(self):
+        self.assertEqual(self.wallet1._web3.eth.getBalance(Web3.toChecksumAddress("9f189536def35811e1a759860672fe49a4f89e94")), 10)
+    
+    def test_poa_emulator_account(self):
+        accounts = []
+        for i in range(5,9):
+            accounts.extend(EthAccount.createEmulatorAccountsFromMnemonic(i, mnemonic="great awesome fun seed security lab protect system network prevent attack future", balance=32*EthUnit.ETHER.value, total=1, password="admin"))
+        for account in accounts:
+            self.assertTrue(self.wallet1._web3.eth.getBalance(account.address) >= 32*EthUnit.ETHER.value)
+
+    def test_poa_create_accounts(self):
+        accounts = []
+        for index in range(1, 4):
+            accounts.append(EthAccount.createEmulatorAccountFromMnemonic(7, mnemonic="great awesome fun seed security lab protect system network prevent attack future", balance=30*EthUnit.ETHER.value, index=index, password="admin"))
+        
+        for account in accounts:
+            self.assertTrue(self.wallet1._web3.eth.getBalance(account.address) >= 30*EthUnit.ETHER.value)
+
+def get_arguments(argv, mapping):
+    # Remove 1st argument from the list of command line arguments
+    argumentList = argv[1:]
+
+    # Options and long options
+    options = "ht:"
+    long_options = ["help", "times="]
+
+    try:
+        # Parsing argument
+        arguments, values = getopt.getopt(argumentList, options, long_options)
+
+        # checking each argument
+        for arg, value in arguments:
+            if arg in ("-h", "--help"):
+                print ("Usage: test_script.py -t <times>")
+                exit()
+
+            elif arg in ("-t", "--times"):
+                mapping['times'] = int(value)
+
+    except getopt.error as err:
+        print (str(err))
+
+def printLog(*args, **kwargs):
+    print(*args, **kwargs)
+    with open('./test_log/log.txt','a') as file:
+        print(*args, **kwargs, file=file)
+
+if __name__ == "__main__":
+    options = {}
+    options['times'] = 1  # default sleeping time
+    get_arguments(sys.argv, options)    
+    result = []
+
+    os.system("rm -rf test_log")
+    os.system("mkdir test_log")
+    
+    for i in range(options['times']):
+        os.system("mkdir ./test_log/test_%d"%i)
+        test_suite = ut.TestSuite()
+        test_suite.addTest(MultipleChainsTestCase('test_poa_chain_connection'))
+        test_suite.addTest(MultipleChainsTestCase('test_poa_chain_id'))
+        test_suite.addTest(MultipleChainsTestCase('test_poa_send_transaction'))
+        test_suite.addTest(MultipleChainsTestCase('test_poa_chain_consensus'))
+        test_suite.addTest(MultipleChainsTestCase('test_poa_peer_counts'))
+        test_suite.addTest(MultipleChainsTestCase('test_poa_emulator_account'))
+        test_suite.addTest(MultipleChainsTestCase('test_poa_create_accounts'))
+        test_suite.addTest(MultipleChainsTestCase('test_import_account'))
+
+        res = ut.TextTestRunner(verbosity=2).run(test_suite)
+
+        succeed = "succeed" if res.wasSuccessful() else "failed"
+        os.system("mv ./test_log/log ./test_log/test_%d/log_%s"%(i, succeed))
+
+        printLog("Emulator Down")
+        result.append(res)
+
+    
+    for count, res in enumerate(result):
+        printLog("==========Test #%d========="%count)
+        num, errs, fails = res.testsRun, len(res.errors), len(res.failures)
+        printLog("score: %d of %d (%d errors, %d failures)" % (num - (errs+fails), num, errs, fails))
+        
---
+++ test/ethereum/POA/emulator-code/blockchain.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+from seedemu import *
+
+CustomGenesisFileContent = '''\
+{
+        "nonce":"0x0",
+        "timestamp":"0x621549f1",
+        "parentHash":"0x0000000000000000000000000000000000000000000000000000000000000000",
+        "extraData":"0x",
+        "gasLimit":"0x80000000",
+        "difficulty":"0x0",
+        "mixhash":"0x0000000000000000000000000000000000000000000000000000000000000000",
+        "coinbase":"0x0000000000000000000000000000000000000000",
+        "number": "0x0",
+        "gasUsed": "0x0",
+        "baseFeePerGas": null,
+        "config": {
+            "chainId": 11,
+            "homesteadBlock": 0,
+            "eip150Block": 0,
+            "eip150Hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
+            "eip155Block": 0,
+            "eip158Block": 0,
+            "byzantiumBlock": 0,
+            "constantinopleBlock": 0,
+            "petersburgBlock": 0,
+            "istanbulBlock": 0,
+            "ethash": {
+            }
+        },
+        "alloc": {
+        }
+}
+'''
+
+emu = Makers.makeEmulatorBaseWith5StubASAndHosts(1)
+
+# Create the Ethereum layer
+# saveState=True: will set the blockchain folder using `volumes`, 
+# so the blockchain data will be preserved when containers are deleted.
+eth = EthereumService()
+
+# Create the 2 Blockchain layers, which is a sub-layer of Ethereum layer
+# Need to specify chainName and consensus when create Blockchain layer.
+
+# blockchain is a POA based blockchain 
+blockchain = eth.createBlockchain(chainName="POA", consensus=ConsensusMechanism.POA)
+
+# Create blockchain nodes (POA Ethereum)
+e1 = blockchain.createNode("poa-eth1")
+e2 = blockchain.createNode("poa-eth2")
+e3 = blockchain.createNode("poa-eth3")
+e4 = blockchain.createNode("poa-eth4")
+e5 = blockchain.createNode("poa-eth5")
+
+e1.setBootNode(True).unlockAccounts().startMiner()
+e1.enableGethWs().setGethWsPort(8541)
+e1.enableGethHttp()
+
+e2.unlockAccounts().startMiner()
+e3.createAccounts(total=3, balance=30, unit=EthUnit.ETHER, password="admin")
+
+e5.setNoDiscover()
+
+emu.getVirtualNode('poa-eth1').setDisplayName('Ethereum-POA-1')
+emu.getVirtualNode('poa-eth2').setDisplayName('Ethereum-POA-2')
+emu.getVirtualNode('poa-eth3').setDisplayName('Ethereum-POA-3')
+emu.getVirtualNode('poa-eth4').setDisplayName('Ethereum-POA-4')
+emu.getVirtualNode('poa-eth5').setDisplayName('Ethereum-POA-5')
+
+
+# Binding virtual nodes to physical nodes
+emu.addBinding(Binding('poa-eth1', filter = Filter(asn = 150, nodeName='host_0')))
+emu.addBinding(Binding('poa-eth2', filter = Filter(asn = 151, nodeName='host_0')))
+emu.addBinding(Binding('poa-eth3', filter = Filter(asn = 152, nodeName='host_0')))
+emu.addBinding(Binding('poa-eth4', filter = Filter(asn = 153, nodeName='host_0')))
+emu.addBinding(Binding('poa-eth5', filter = Filter(asn = 154, nodeName='host_0')))
+
+# Add the layer and save the component to a file
+emu.addLayer(eth)
+emu.dump('component-blockchain.bin')
+
+emu.render()
+
+docker = Docker(etherViewEnabled=True)
+
+# Add the 'rafaelawon/seedemu-lighthouse-base' custom image from dockerhub.
+# This image contains custom lighthouse software.
+docker.addImage(DockerImage('rafaelawon/seed-geth-base:v1.10.26', [], local=False), priority=-1)
+
+base = emu.getLayer('Base')
+
+# Get the physical nodes of all hosts from Base layer.
+# The name of physical nodes generated from Makers.makeEmulatorBaseWith10StubASAndHosts() is 'host_{}'
+# Base::getNodesByName('host') returns the physical nodes whose name starts with 'host'.
+hosts = base.getNodesByName('host')
+
+# Set all host nodes to use the custom 'seedemu-lighthouse-base' image.
+for host in hosts:
+   docker.setImageOverride(host, 'rafaelawon/seed-geth-base:v1.10.26')
+
+# If output directory exists and override is set to false, we call exit(1)
+# updateOutputdirectory will not be called
+emu.compile(docker, './output')
