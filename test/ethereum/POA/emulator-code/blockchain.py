b7d69e35aa742e4615419a0faeff1cdd9fe25739
wonkr <kyungrok.won@gmail.com>
2023-02-25 22:14:41 -0500

    update POA test

--- test/ethereum/POA/emulator-code/blockchain.py
+++ test/ethereum/POA/emulator-code/blockchain.py
@@ -3,47 +3,10 @@
 
 from seedemu import *
 
-CustomGenesisFileContent = '''\
-{
-        "nonce":"0x0",
-        "timestamp":"0x621549f1",
-        "parentHash":"0x0000000000000000000000000000000000000000000000000000000000000000",
-        "extraData":"0x",
-        "gasLimit":"0x80000000",
-        "difficulty":"0x0",
-        "mixhash":"0x0000000000000000000000000000000000000000000000000000000000000000",
-        "coinbase":"0x0000000000000000000000000000000000000000",
-        "number": "0x0",
-        "gasUsed": "0x0",
-        "baseFeePerGas": null,
-        "config": {
-            "chainId": 11,
-            "homesteadBlock": 0,
-            "eip150Block": 0,
-            "eip150Hash": "0x0000000000000000000000000000000000000000000000000000000000000000",
-            "eip155Block": 0,
-            "eip158Block": 0,
-            "byzantiumBlock": 0,
-            "constantinopleBlock": 0,
-            "petersburgBlock": 0,
-            "istanbulBlock": 0,
-            "ethash": {
-            }
-        },
-        "alloc": {
-        }
-}
-'''
-
 emu = Makers.makeEmulatorBaseWith5StubASAndHosts(1)
 
 # Create the Ethereum layer
-# saveState=True: will set the blockchain folder using `volumes`, 
-# so the blockchain data will be preserved when containers are deleted.
 eth = EthereumService()
-
-# Create the 2 Blockchain layers, which is a sub-layer of Ethereum layer
-# Need to specify chainName and consensus when create Blockchain layer.
 
 # blockchain is a POA based blockchain 
 blockchain = eth.createBlockchain(chainName="POA", consensus=ConsensusMechanism.POA)
@@ -62,7 +25,7 @@ e1.enableGethHttp()
 e2.importAccount(keyfilePath='./resources/keyfile_to_import', password="admin", balance=10)
 e2.unlockAccounts().startMiner()
 e3.createAccounts(total=3, balance=30, unit=EthUnit.ETHER, password="admin")
-
+e4.unlockAccounts().startMiner()
 e5.setNoDiscover()
 
 emu.getVirtualNode('poa-eth1').setDisplayName('Ethereum-POA-1')
@@ -79,9 +42,8 @@ emu.addBinding(Binding('poa-eth3', filter = Filter(asn = 152, nodeName='host_0')
 emu.addBinding(Binding('poa-eth4', filter = Filter(asn = 153, nodeName='host_0')))
 emu.addBinding(Binding('poa-eth5', filter = Filter(asn = 154, nodeName='host_0')))
 
-# Add the layer and save the component to a file
+# Add the layer
 emu.addLayer(eth)
-emu.dump('component-blockchain.bin')
 
 emu.render()
 
