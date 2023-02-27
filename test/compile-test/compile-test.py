5fbf8941c1351f2f78fbcd254950ae1fbd33917b
wonkr <kyungrok.won@gmail.com>
2023-02-27 12:23:54 -0500

    fix test script

--- test/compile-test/compile-test.py
+++ test/compile-test/compile-test.py
@@ -43,14 +43,15 @@ class CompileTest(ut.TestCase):
         cls.init_cwd = os.getcwd()
 
         cls.path = "../../examples"
+        os.chdir(cls.path)
         for dir, (scripts, outputs) in cls.test_list.items():
-            path = os.path.join(cls.path, dir)
-            os.chdir(path)
+            os.chdir(dir)
             file_list = os.listdir(os.curdir)
             for output in outputs:
                 if output in file_list: 
                     if os.path.isdir(os.path.join(path, output)):   shutil.rmtree(output)
                     else:   os.remove(output)
+            os.chdir("../")
         os.chdir(cls.init_cwd)
         return super().setUpClass()
         
