f4b5bc2081abac56399951cbc86a4b10b2866d04
wonkr <kyungrok.won@gmail.com>
2024-03-28 23:51:54 -0500

    update faucet example

--- examples/B12-ethereum-faucet/blockchain.py
+++ examples/B12-ethereum-faucet/blockchain.py
@@ -3,6 +3,8 @@
 
 from seedemu import *
 import sys
+from eth_account import Account
+
 
 emu = Makers.makeEmulatorBaseWith10StubASAndHosts(1)
 
@@ -55,21 +57,48 @@ emu.addBinding(Binding('poa-eth8', filter = Filter(asn = 163, nodeName='host_0')
 
 # Faucet Service
 
-# Enable http connection on e5 geth
-e5.enableGethHttp()
+
+
+# Generate a new Ethereum account for the Owner of faucet account
+faucet_owner_account = Account.create()
+
+# Add the ethereum using the EthereumService
+blockchain1.addLocalAccount(address=faucet_owner_account.address, balance=1000, unit=EthUnit.ETHER)
 
 faucetService = FaucetService()
 faucet:FaucetServer = faucetService.install('faucet')
-faucet.linkEthNodeByName('poa-eth5')
+faucet.setOwnerPrivateKey(keyString=faucet_owner_account.privateKey.hex(), isEncrypted=False)
+faucet.setPort(80)
+faucet.setChainId(blockchain1.getChainId())
+faucet.setConsensusMechanism(ConsensusMechanism.POA)
+
+# if user know the url of the eth node,
+# user can use faucet::setRpcUrl method instead.
+# But in this scenario, we have no information of the ip address of eth5 node.
+# So, we are using FaucetService::setRpcUrlByVirtualNodeName method to link the faucet service to the ethnode specified.
+# Make sure that eth5 node has enabled geth.
+
+faucet.setRpcUrlByVirtualNodeName('poa-eth5')
+# Enable http connection on e5 geth
+e5.enableGethHttp()
+
 faucet.fund('0x72943017a1fa5f255fc0f06625aec22319fcd5b3', 2)
 faucet.fund('0x5449ba5c5f185e9694146d60cfe72681e2158499', 5)
 
 emu.addBinding(Binding('faucet', filter=Filter(asn=154, nodeName='host_0')))
 
+###############################################################
+# Faucet User Server Example => chainlink
+faucetUser = FaucetUserService()
+faucetUser.setFaucetServerInfo(vnode = 'faucet', port=80)
+faucetUser.install('faucet_user')
+
+emu.addBinding(Binding('faucet_user', filter=Filter(asn=164, nodeName='host_0')))
 
 # Add the layer and save the component to a file
 emu.addLayer(eth)
 emu.addLayer(faucetService)
+emu.addLayer(faucetUser)
 emu.dump('component-blockchain.bin')
 
 emu.render()
