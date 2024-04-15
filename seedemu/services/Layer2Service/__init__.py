975f1fcc78119a069b9b32916cc5453dba601c6b
Alan Yang <hua.void@gmail.com>
2024-04-15 19:44:31 +0000

    fix: fix wildcard import issue

--- seedemu/services/Layer2Service/__init__.py
+++ seedemu/services/Layer2Service/__init__.py
@@ -3,5 +3,5 @@
 
 from __future__ import annotations
 
-from .Layer2Service import *
-from .Layer2Template import *
+from .Layer2Service import Layer2Server, Layer2Service, Layer2Blockchain
+from .Layer2Template import L2Component, L2Account, L2Config, L2Node, Layer2Template
