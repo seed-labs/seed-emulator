4bcb61c097b881f2ae9c303a0a48557c891de67b
akibjawad <a.j.nafis@gmail.com>
2024-04-15 21:05:00 -0400

    updated as-example for arm

--- examples/A00-simple-as/simple-as.py
+++ examples/A00-simple-as/simple-as.py
@@ -87,4 +87,4 @@ emu.render()
 ###############################################################################
 # Compilation
 
-emu.compile(Docker(), './output')
+emu.compile(Docker(internetMapEnabled=True, platform=Platform.ARM64), './output')
