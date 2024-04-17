1f838f215d822f00368f8b42c9723ce6e2c8baca
Alan Yang <hua.void@gmail.com>
2024-04-17 04:38:51 +0000

    docs: add optional l2node methods demos

--- examples/layer2/layer2WithService.py
+++ examples/layer2/layer2WithService.py
@@ -111,8 +111,10 @@ l2_1 = l2Bkc.createNode("l2-1", EthereumLayer2Node.SEQUENCER)
 l2_2 = l2Bkc.createNode("l2-2").setL1VNode("poa-eth6", e6.getGethHttpPort())
 
 # Default type of the node is the non-sequencer node
-l2_3 = l2Bkc.createNode("l2-3")
-l2_4 = l2Bkc.createNode("l2-4")
+# Set the http port for l2-3 (default: 8545)
+l2_3 = l2Bkc.createNode("l2-3").setHttpPort(9545)
+# Set the ws port for l2-4  (default: 8546)
+l2_4 = l2Bkc.createNode("l2-4").setWSPort(8547)
 
 # Set the deployer node, which is used to deploy the smart contract
 # Only one deployer node is allowed in a layer2 blockchain
