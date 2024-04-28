85bc6b2cd8551af7632a5341ce1e33e74ccf03b1
Kevin Du <wedu@syr.edu>
2024-04-28 18:01:01 -0400

    updated the example code and instructions

--- examples/C06-pki/pki-with-dns.py
+++ examples/C06-pki/pki-with-dns.py
@@ -5,49 +5,47 @@ from seedemu.compiler import Docker
 from seedemu.core import Binding, Emulator, Filter, Action
 from seedemu.layers import Base
 from seedemu.services import DomainNameService, CAService, WebService, WebServer, RootCAStore
-from basenet import dump
+from basenetwithDNS import dump
 
 emu = Emulator()
 basenet = dump()
 emu.load(basenet)
 
 base: Base = emu.getLayer('Base')
+dns: DomainNameService = emu.getLayer('DomainNameService')
 
-# Create a physical node for CA servers
-as150 = base.getAutonomousSystem(150)
-as150.createHost('ca').joinNetwork('net0').addHostName('seedCA.net')
+caStore = RootCAStore(caDomain='ca.internal')
+ca = CAService(caStore)
+web = WebService()
 
-# Create two physical nodes for web servers
-as151 = base.getAutonomousSystem(151)
-as151.createHost('web1').joinNetwork('net0').addHostName('example32.com')
-as151.createHost('web2').joinNetwork('net0').addHostName('bank32.com')
-
-# Create and configure CA server vnodes
-caStore = RootCAStore(caDomain='seedCA.net')
-ca  = CAService(caStore)
 ca.setCertDuration("2160h")
+
+# Add records to zones
+dns.getZone('ca.internal.').addRecord('@ A 10.150.0.7')
+dns.getZone('user1.internal.').addRecord('@ A 10.151.0.7')
+dns.getZone('user2.internal.').addRecord('@ A 10.151.0.8')
+
 ca.install('ca-vnode')
 ca.installCACert()
 
-# Create and configure web server vnodes
-web = WebService()
+as150 = base.getAutonomousSystem(150)
+# Do not install the CA cert on the CA host
+host_ca = as150.createHost('ca').joinNetwork('net0', address='10.150.0.7')
+
+as151 = base.getAutonomousSystem(151)
+host_web = as151.createHost('web1').joinNetwork('net0', address='10.151.0.7')
+host_web = as151.createHost('web2').joinNetwork('net0', address='10.151.0.8')
+
 webServer1: WebServer = web.install('web1-vnode')
-webServer1.setServerNames(['example32.com'])
+webServer1.setServerNames(['user1.internal'])
 webServer1.useCAService(ca).enableHTTPS()
-webServer1.setIndexContent("<h1>Web server at example32.com</h1>")
-
 webServer2: WebServer = web.install('web2-vnode')
-webServer2.setServerNames(['bank32.com'])
+webServer2.setServerNames(['user2.internal'])
 webServer2.useCAService(ca).enableHTTPS()
-webServer2.setIndexContent("<h1>Web server at bank32.com</h1>")
-
-# Bind vnodes to physical nodes
 emu.addBinding(Binding('ca-vnode', filter=Filter(nodeName='ca'), action=Action.FIRST))
 emu.addBinding(Binding('web1-vnode', filter=Filter(nodeName='web1'), action=Action.FIRST))
 emu.addBinding(Binding('web2-vnode', filter=Filter(nodeName='web2'), action=Action.FIRST))
 
-
-# Add layers, render and compile 
 emu.addLayer(ca)
 emu.addLayer(web)
 
