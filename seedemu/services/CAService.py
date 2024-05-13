64b2ab7f920e0eb8b7a7b667619cbd85ea10836e
wonkr <kyungrok.won@gmail.com>
2024-05-13 08:08:39 -0500

    fix

--- seedemu/services/CAService.py
+++ seedemu/services/CAService.py
@@ -403,8 +403,8 @@ class RootCAStore:
 
         # To change owner of the folder
         container = BuildtimeDockerImage(f"alpine:latest").container()
-        container.user(f"{os.getuid()}:{os.getuid()}").mountVolume("/tmp", "/tmp")
-        # container.run('chown -R 1000:1000 /tmp')
+        container.mountVolume(self.__caDir, "/tmp")
+        container.run(f"chown -R {os.getuid()}:{os.getuid()} /tmp")
         # docker run -it --rm -v /tmp:/tmp ubuntu:latest chown -R 1000:1000 /tmp
         self.__initialized = True
 
