5ce61f1121b09b728e4a1a9b42eebd66e6d54061
Kevin Du <wedu@syr.edu>
2024-05-17 15:43:18 -0400

    adjust the parameters

--- examples/internet/B27_traffic_generator/2-scapy-traffic-generator/scapy-traffic-generator.py
+++ examples/internet/B27_traffic_generator/2-scapy-traffic-generator/scapy-traffic-generator.py
@@ -11,29 +11,30 @@ def run(dumpfile=None):
 
     emu = Emulator()
 
-    # Run the pre-built components
+    # Run and load the pre-built components
     mini_internet.run(dumpfile='./base-internet.bin')
-    
-    # Load and merge the pre-built components 
     emu.load('./base-internet.bin')
     
     base = emu.getLayer('Base')
 
-    etc_hosts = EtcHosts()
-
     traffic_service = TrafficService()
-    traffic_service.install('scapy-generator', TrafficServiceType.SCAPY_GENERATOR, log_file="/root/scapy-logs").addReceivers(hosts=["10.164.0.0/24", "10.170.0.0/24"])
+    traffic_service.install(
+           'scapy-generator', 
+           TrafficServiceType.SCAPY_GENERATOR, 
+           log_file="/root/scapy-logs"
+    ).addReceivers(hosts=["10.164.0.0/24", "10.170.0.0/24"])
 
     # Add hosts to AS-150
     as150 = base.getAutonomousSystem(150)
     as150.createHost('scapy-generator').joinNetwork('net0')
 
     # Binding virtual nodes to physical nodes
-    emu.addBinding(Binding('scapy-generator', filter = Filter(asn = 150, nodeName='scapy-generator')))
+    emu.addBinding(Binding('scapy-generator', 
+          filter = Filter(asn = 150, nodeName='scapy-generator')))
 
     # Add the layers
     emu.addLayer(traffic_service)
-    emu.addLayer(etc_hosts)
+    emu.addLayer(EtcHosts())
     
     if dumpfile is not None:
        # Save it to a file, so it can be used by other emulators
--- examples/internet/B27_traffic_generator/3-hybrid-traffic-generator/hybrid-traffic-generator.py
+++ examples/internet/B27_traffic_generator/3-hybrid-traffic-generator/hybrid-traffic-generator.py
@@ -23,7 +23,10 @@ def run(dumpfile=None):
 
     traffic_service = TrafficService()
     traffic_service.install('hybrid-receiver', TrafficServiceType.HYBRID_RECEIVER)
-    traffic_service.install('hybrid-generator', TrafficServiceType.HYBRID_GENERATOR).addReceivers(hosts=["hybrid-receiver"])
+    traffic_service.install(
+            'hybrid-generator', 
+            TrafficServiceType.HYBRID_GENERATOR
+    ).addReceivers(hosts=["hybrid-receiver"])
 
     # Add hosts to AS-150
     as150 = base.getAutonomousSystem(150)
@@ -35,8 +38,10 @@ def run(dumpfile=None):
     as162.createHost('hybrid-receiver').joinNetwork('net0')
 
     # Binding virtual nodes to physical nodes
-    emu.addBinding(Binding('hybrid-generator', filter = Filter(asn = 150, nodeName='hybrid-generator')))
-    emu.addBinding(Binding('hybrid-receiver', filter = Filter(asn = 162, nodeName='hybrid-receiver')))
+    emu.addBinding(Binding('hybrid-generator', 
+                   filter = Filter(asn = 150, nodeName='hybrid-generator')))
+    emu.addBinding(Binding('hybrid-receiver', 
+                   filter = Filter(asn = 162, nodeName='hybrid-receiver')))
 
     # Add the layers
     emu.addLayer(traffic_service)
