561d0b401a1680136a427c5cec2b3b67840f347f
Kevin Du <wedu@syr.edu>
2024-06-01 22:05:27 -0400

    modified some variable names

--- seedemu/services/ChainlinkService/ChainlinkTemplates/files/deploy_oracle_contract.py
+++ seedemu/services/ChainlinkService/ChainlinkTemplates/files/deploy_oracle_contract.py
@@ -8,8 +8,8 @@ import re
 import requests
 import json
 
-rpc_url = "http://{rpc_ip}:{rpc_port}"
-faucet_ip = "http://{faucet_ip}:{faucet_port}"
+rpc_url    = "http://{eth_server_ip}:{eth_server_http_port}"
+faucet_url = "http://{faucet_ip}:{faucet_port}"
 
 # Change the work folder to where the program is
 abspath = os.path.abspath(__file__)
@@ -53,7 +53,7 @@ timeout = 600
 start_time = time.time()
 while True:
     try:
-        response = requests.get(faucet_ip)
+        response = requests.get(faucet_url)
         if response.status_code == 200:
             logging.info("faucet server connection succeed.")
             break
@@ -68,29 +68,32 @@ while True:
 
 
 def send_fundme_request(account_address):
-	data = {{'address': account_address, 'amount': 10}}
-	logging.info(data)
-	request_ip = "http://{faucet_ip}:{faucet_port}/fundme"
-	try:
-		response = requests.post(request_ip, headers={{"Content-Type": "application/json"}}, data=json.dumps(data))
-		logging.info(response)
-		if response.status_code == 200:
-			api_response = response.json()
-			message = api_response['message']
-			if message:
-				print(f"Success: {{message}}")
-			else:
-				logging.error("Funds request was successful but the response format is unexpected.")
-		else:
-			api_response = response.json()
-			message = api_response['message']
-			logging.error(f"Failed to request funds from faucet server. Status code: {{response.status_code}} Message: {{message}}")
-			# Send another request
-			logging.info("Sending another request to faucet server.")
-			send_fundme_request(owner_address)
-	except Exception as e:
-		logging.error(f"An error occurred: {{str(e)}}")
-		exit()
+    data = {{'address': account_address, 'amount': 10}}
+    request_ip = "http://{faucet_ip}:{faucet_port}/fundme"
+    try:
+      response = requests.post(request_ip, 
+                 headers={{"Content-Type": "application/json"}}, 
+                 data=json.dumps(data))
+      logging.info(response)
+      if response.status_code == 200:
+         api_response = response.json()
+         message = api_response['message']
+         if message:
+             print(f"Success: {{message}}")
+         else:
+             logging.error("Funds request was successful but the response format is unexpected.")
+      else:
+        api_response = response.json()
+        message = api_response['message']
+        logging.error(f"Failed to request funds from faucet server. Status code: {{response.status_code}} Message: {{message}}")
+
+        # Send another request
+        logging.info("Sending another request to faucet server.")
+	send_fundme_request(owner_address)
+
+    except Exception as e:
+        logging.error(f"An error occurred: {{str(e)}}")
+        exit()
 
 # Send /fundme request to faucet server
 send_fundme_request(owner_address)
