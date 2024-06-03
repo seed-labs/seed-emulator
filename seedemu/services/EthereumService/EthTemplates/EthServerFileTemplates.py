2eaf0f33241e798c7eeb768f92e5687d79c3b720
Kevin Du <wedu@syr.edu>
2024-06-02 21:09:37 -0400

    removed some entries

--- seedemu/services/EthereumService/EthTemplates/EthServerFileTemplates.py
+++ seedemu/services/EthereumService/EthTemplates/EthServerFileTemplates.py
@@ -14,21 +14,18 @@ def get_file_content(filename):
 
 EthServerFileTemplates: Dict[str, str] = {
         'bootstrapper':        get_file_content("files_ethereum/bootstrapper.sh"),
-        'beacon_bootstrapper': get_file_content("files_ethereum/beacon_bootstrapper.sh"),
-        'helper_class_py':     get_file_content("files_ethereum/EthereumHelper.py")
+        'beacon_bootstrapper': get_file_content("files_ethereum/beacon_bootstrapper.sh")
 }
 
 UtilityServerFileTemplates: Dict[str, str] = {
         'fund_account':    get_file_content("files_utility/fund_account.py"),
         'deploy_contract': get_file_content("files_utility/deploy_contract.py"),
-        'info_server':     get_file_content("files_utility/info_server.py"),
-        'helper_class_py': get_file_content("files_utility/UtilityServerHelper.py")
+        'info_server':     get_file_content("files_utility/info_server.py")
 }
 
 FaucetServerFileTemplates: Dict[str, str] = {
         'faucet_server':   get_file_content("files_faucet/faucet_server.py"),
         'fund_script':     get_file_content("files_faucet/fund_script.sh"),
-        'helper_class_py': get_file_content("files_faucet/FaucetHelper.py"),
         'faucet_url':      "http://{address}:{port}/",
         'faucet_fund_url': "http://{address}:{port}/fundme",
         'fund_curl': "curl -X POST -d 'address={recipient}&amount={amount}' http://{address}:{port}/fundme"
