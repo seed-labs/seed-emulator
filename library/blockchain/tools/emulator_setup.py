33158e12a66a707e033fea8f48669f65c8d2090d
Kevin Du <wedu@syr.edu>
2024-11-17 22:49:56 -0500

    small changes

--- library/blockchain/tools/emulator_setup.py
+++ library/blockchain/tools/emulator_setup.py
@@ -1,8 +1,8 @@
 
 chain_id    = 1337
-eth_url     = "http://10.150.0.75:8545"
-faucet_url  = "http://10.150.0.81:80"
-utility_url = "http://10.150.0.82:5000"
+eth_url     = "http://10.151.0.73:8545"
+faucet_url  = "http://10.161.0.73:80"
+utility_url = "http://10.162.0.71:5000"
 
 emu_sender_address = '0x2e2e3a61daC1A2056d9304F79C168cD16aAa88e9'
 emu_sender_key     = '20aec3a7207fcda31bdef03001d9caf89179954879e595d9a190d6ac8204e498' 
