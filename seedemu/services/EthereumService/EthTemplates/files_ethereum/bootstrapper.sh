daf54022e5d44722e630cac5b6bad17a6484eea0
Muhammad Jamshaid Ghaffar <muhammadjamshaid89@gmail.com>
2025-07-06 16:04:28 +0500

    changed geth bootnode binary to bootnode

--- seedemu/services/EthereumService/EthTemplates/files_ethereum/bootstrapper.sh
+++ seedemu/services/EthereumService/EthTemplates/files_ethereum/bootstrapper.sh
@@ -2,7 +2,7 @@
 while read -r node; do {
     let count=0
     ok=true
-    until curl -sHf http://$node:8545/eth-enode-url > /dev/null; do {
+    until curl -sHf http://$node:8088/eth-enode-url > /dev/null; do {
         echo "eth: node $node not ready, waiting..."
         sleep 3
         let count++
