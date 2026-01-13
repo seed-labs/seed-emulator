d22ea5c0818d166bca85a9190051da306105f5ec
Kevin Du <kevin@seed>
2026-01-13 16:54:19 +0800

    removed -d

--- tools/DemoSystem/yesterday_once_more/02_morris_worm/emulator/container_files/z_build.sh
+++ tools/DemoSystem/yesterday_once_more/02_morris_worm/emulator/container_files/z_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker compose build -d morris-worm-base
+docker compose build morris-worm-base
 ls | grep -Ev '.yml$|^dummies$|^morris|^z_start|^z_build' | xargs -n20 -exec docker compose build
