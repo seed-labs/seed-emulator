ef6de458c3c0a6a31fb0e1fb51a6bd9f1b8d86b0
KkaiFang <1171367990@qq.com>
2026-04-13 16:08:31 +0800

    Optimize the code structure and add comments.

--- examples/blockchain/D01_ethereum_pos/ethereum_pos.py
+++ examples/blockchain/D01_ethereum_pos/ethereum_pos.py
@@ -8,7 +8,9 @@ from eth_account import Account
 
 DOMAIN = ".net"
 
-def run(dumpfile = None, total_beacon_nodes=3, vc_per_beacon=2):
+def run(dumpfile = None, total_beacon_nodes=3, vc_per_beacon=3):
+    ###############################################################################
+    # Set the platform information
     if dumpfile is None:
         script_name = os.path.basename(__file__)
         if len(sys.argv) == 1:
@@ -24,28 +26,29 @@ def run(dumpfile = None, total_beacon_nodes=3, vc_per_beacon=2):
         else:
             print(f"Usage:  {script_name} amd|arm")
             sys.exit(1)
+
+    # Configure the number of nodes
     geth_node_number = total_beacon_nodes
     beacon_node_number = total_beacon_nodes
     vc_node_number = vc_per_beacon * beacon_node_number
-    beaconsetup_node_number = 1
-    total_number_of_nodes = geth_node_number + beacon_node_number + vc_node_number + beaconsetup_node_number
-    total_stub_as = 10
-    hosts_per_stub_as = math.ceil(total_number_of_nodes / total_stub_as)
-    emu = Makers.makeEmulatorBaseWith10StubASAndHosts(hosts_per_stub_as=hosts_per_stub_as)
-    print(f"Number of eth nodes per stub AS: {hosts_per_stub_as}")
+    # Create Emulator Base with 10 Stub AS (150-154, 160-164) using Makers utility method.
+    # hosts_per_stub_as=3 : create 3 hosts per one stub AS.
+    # It will create hosts(physical node) named `host_{}`.format(counter), counter starts from 0. 
+    hosts_per_stub_as = 3
+    emu = Makers.makeEmulatorBaseWith10StubASAndHosts(hosts_per_stub_as = hosts_per_stub_as)
+    
+    # Create the Ethereum layer
     eth = EthereumService()
+    # Create the Blockchain layer which is a sub-layer of Ethereum layer.
+    # chainName="pos": set the blockchain name as "pos"
+    # consensus="ConsensusMechnaism.POS" : set the consensus of the blockchain as "ConsensusMechanism.POS".
+    # supported consensus option: ConsensusMechanism.POA, ConsensusMechanism.POW, ConsensusMechanism.POS
     blockchain = eth.createBlockchain(chainName="pos", consensus=ConsensusMechanism.POS)
-    accounts_total  = 1000
-    pre_funded_amount = 1000000
-    mnemonic = "gentle always fun glass foster produce north tail security list example gain"
-    Account.enable_unaudited_hdwallet_features()
-    for i in range(accounts_total):
-         account = Account.from_mnemonic(mnemonic, account_path=f"m/44'/60'/0'/0/{i}")
-         blockchain.addLocalAccount(address=account.address, balance=pre_funded_amount)
     asns = [150, 151, 152, 153, 154, 160, 161, 162, 163, 164]
     geth_nodes: List[PoSGethServer] = []
     beacon_nodes: List[PoSBeaconServer] = []
     vc_nodes: List[PoSVcServer] =[]
+    # Create the BeaconSetupNode
     beaconsetupServer: PoSBeaconSetupServer = blockchain.createBeaconSetupNode(f"BeaconSetupNode")
     emu.getVirtualNode(f'BeaconSetupNode').setDisplayName('Ethereum-POS-BeaconSetup')
     for i in range(geth_node_number):
@@ -54,14 +57,20 @@ def run(dumpfile = None, total_beacon_nodes=3, vc_per_beacon=2):
         gethServer.appendClassName(f'Ethereum-POS-Geth-{i+1}')
         geth_nodes.append(gethServer)
         emu.getVirtualNode(f'gethnode{i}').setDisplayName(f'Ethereum-POS-Geth-{i+1}')
+    
+    # Create Beacon nodes and connect to Geth nodes
     for i in range(beacon_node_number):
         beaconServer: PoSBeaconServer = blockchain.createBeaconNode(f"beaconnode{i}")
         beaconServer.appendClassName(f'Ethereum-POS-Beacon-{i+1}')
         beaconServer.connectToGethNode(f"gethnode{(i+1)%len(geth_nodes)}")
         beacon_nodes.append(beaconServer)
         emu.getVirtualNode(f'beaconnode{i}').setDisplayName(f'Ethereum-POS-Beacon-{i+1}')
+    
+    # Set boot nodes
     geth_nodes[0].setBootNode(True)
     beacon_nodes[0].setBootNode(True)
+    
+    # Create Validator nodes and connect to Beacon nodes
     for i in range(vc_node_number):
         VcServer: PoSVcServer=blockchain.createVcNode(f"vcnode{i}")
         VcServer.appendClassName(f'Ethereum-POS-Validator-{i+1}')
@@ -69,9 +78,8 @@ def run(dumpfile = None, total_beacon_nodes=3, vc_per_beacon=2):
         VcServer.enablePOSValidatorAtGenesis()
         vc_nodes.append(VcServer)
         emu.getVirtualNode(f'vcnode{i}').setDisplayName(f'Ethereum-POS-Validator-{i+1}')
-    assign_index = 0
-    total_nodes = len(geth_nodes) + len(beacon_nodes) + len(vc_nodes)
-
+    
+    # Create the Faucet server
     faucet:FaucetServer = blockchain.createFaucetServer(
                 vnode='faucet',
                 port=80,
@@ -80,6 +88,7 @@ def run(dumpfile = None, total_beacon_nodes=3, vc_per_beacon=2):
                 max_fund_amount=10)
     faucet.setDisplayName('Faucet')
     faucet.addHostName('faucet' + DOMAIN)
+    # Create the Utility server
     util_server:EthUtilityServer = blockchain.createEthUtilityServer(
                 vnode='utility',
                 port=5000,
@@ -88,20 +97,26 @@ def run(dumpfile = None, total_beacon_nodes=3, vc_per_beacon=2):
     util_server.setDisplayName('UtilityServer')
     util_server.addHostName('utility' + DOMAIN)
     
-    
-    
+    # Add Ethereum service to the emulator
     emu.addLayer(eth)
+    
     # Bind all virtual servers (including faucet/utility) to physical hosts
     for _, servers in blockchain.getAllServerNames().items():
         for server in servers:
             emu.addBinding(Binding(server, filter=Filter(nodeName="host_*"),
                            action=Action.FIRST))
+    
+    # Add /etc/hosts layer
     emu.addLayer(EtcHosts())
+    
+    # Configure IP range for each AS
     base_layer = emu.getLayer('Base')
     for asn in asns:
         as_obj = base_layer.getAutonomousSystem(asn)
         net = as_obj.getNetwork('net0')
         net.setHostIpRange(hostStart=71, hostEnd=199, hostStep=1)
+        
+    # Generate the emulator output
     if dumpfile is not None:
         emu.dump(dumpfile)
     else:
--- examples/blockchain/D20_faucet/faucet.py
+++ examples/blockchain/D20_faucet/faucet.py
@@ -4,30 +4,47 @@
 import sys, os
 from seedemu import *
 from examples.blockchain.D00_ethereum_poa import ethereum_poa
+from examples.blockchain.D01_ethereum_pos import ethereum_pos
 
 ###############################################################################
 # Set the platform information
 script_name = os.path.basename(__file__)
 
-if len(sys.argv) == 1:
-    platform = Platform.AMD64
-elif len(sys.argv) == 2:
-    if sys.argv[1].lower() == 'amd':
-        platform = Platform.AMD64
-    elif sys.argv[1].lower() == 'arm':
-        platform = Platform.ARM64
+consensus = 'poa'
+platform = Platform.AMD64
+if len(sys.argv) >= 2:
+    arg1 = sys.argv[1].lower()
+    if arg1 in ['poa', 'pos']:
+        consensus = arg1
+        if len(sys.argv) == 3:
+            arg2 = sys.argv[2].lower()
+            if arg2 == 'amd':
+                platform = Platform.AMD64
+            elif arg2 == 'arm':
+                platform = Platform.ARM64
+            else:
+                print(f"Usage:  {script_name} [poa|pos] [amd|arm]")
+                sys.exit(1)
+        elif len(sys.argv) > 3:
+            print(f"Usage:  {script_name} [poa|pos] [amd|arm]")
+            sys.exit(1)
     else:
-        print(f"Usage:  {script_name} amd|arm")
-        sys.exit(1)
-else:
-    print(f"Usage:  {script_name} amd|arm")
-    sys.exit(1)
+        if arg1 == 'amd':
+            platform = Platform.AMD64
+        elif arg1 == 'arm':
+            platform = Platform.ARM64
+        else:
+            print(f"Usage:  {script_name} [poa|pos] [amd|arm]")
+            sys.exit(1)
 
 emu = Emulator()
 
-# Run and load the pre-built ethereum component; it is used as the base blockchain
-local_dump_path = './blockchain_poa.bin'
-ethereum_poa.run(dumpfile=local_dump_path, total_accounts_per_node=1)
+if consensus == 'pos':
+    local_dump_path = './blockchain_pos.bin'
+    ethereum_pos.run(dumpfile=local_dump_path)
+else:
+    local_dump_path = './blockchain_poa.bin'
+    ethereum_poa.run(dumpfile=local_dump_path, total_accounts_per_node=1)
 emu.load(local_dump_path)
 
 # Get the faucet server instance
@@ -36,7 +53,7 @@ blockchain  = eth.getBlockchainByName(eth.getBlockchainNames()[0])
 faucet_name = blockchain.getFaucetServerNames()[0]
 faucet      = blockchain.getFaucetServerByName(faucet_name)
 
-# Funding accounts during the build time. The actual funding  
+# Funding accounts during the build time. The actual funding
 # is carried out after the emulator starts
 faucet.fund('0x72943017a1fa5f255fc0f06625aec22319fcd5b3', 2)
 faucet.fund('0x5449ba5c5f185e9694146d60cfe72681e2158499', 5)
@@ -55,4 +72,3 @@ emu.render()
 docker = Docker(etherViewEnabled=True, platform=platform)
 emu.compile(docker, './output', override=True)
 # user_node.print(indent=4)
-
